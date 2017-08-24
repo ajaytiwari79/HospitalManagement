4765afad855859346e92b8b6f681376e03bf6dcd
anilm2 <anil.maurya@oodlestechnologies.com>
2017-08-24 10:56:43 +0530

    inject json converter in rest template

--- kairos-user/src/main/java/com/kairos/UserServiceApplication.java
+++ kairos-user/src/main/java/com/kairos/UserServiceApplication.java
@@ -1,8 +1,9 @@
 package com.kairos;
 
-import com.kairos.utils.userContext.UserContextInterceptor;
+import com.kairos.util.userContext.UserContextInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -11,13 +12,15 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -31,7 +34,7 @@ import java.util.List;
 @EnableNeo4jRepositories(basePackages = {"com.kairos.persistence.repository"})
 @EnableEurekaClient
 @EnableCircuitBreaker
-public class UserServiceApplication {
+public class UserServiceApplication extends WebMvcConfigurerAdapter{
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
@@ -66,19 +69,34 @@ public class UserServiceApplication {
 
 		}
 
+
+	@Bean
+	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+		MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+		mappingJackson2HttpMessageConverter.setPrettyPrint(true);
+		return mappingJackson2HttpMessageConverter;
+	}
+	@Bean
+	public MappingJackson2XmlHttpMessageConverter mappingJackson2XmlHttpMessageConverter() {
+		MappingJackson2XmlHttpMessageConverter mappingJackson2XmlHttpMessageConverter = new MappingJackson2XmlHttpMessageConverter();
+		mappingJackson2XmlHttpMessageConverter.setPrettyPrint(true);
+		return mappingJackson2XmlHttpMessageConverter;
+	}
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		super.configureMessageConverters(converters);
+		converters.add(mappingJackson2HttpMessageConverter());
+		converters.add(mappingJackson2XmlHttpMessageConverter());
+	}
+
     @LoadBalanced
 	@Primary
 	@Bean
-	public RestTemplate getCustomRestTemplate() {
-		RestTemplate template = new RestTemplate();
-		List interceptors = template.getInterceptors();
-		if (interceptors == null) {
-			template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
-		} else {
-			interceptors.add(new UserContextInterceptor());
-			template.setInterceptors(interceptors);
-		}
-
+	public RestTemplate getCustomRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+		RestTemplate template =restTemplateBuilder
+				.interceptors(new UserContextInterceptor())
+				.messageConverters(mappingJackson2HttpMessageConverter())
+				.build();
 		return template;
 	}
 	}
