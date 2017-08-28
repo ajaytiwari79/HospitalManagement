48d6e108dae8a8fb8edd2751bc4906a372edae99
anilm2 <anil.maurya@oodlestechnologies.com>
2017-08-28 12:20:09 +0530

    implements Rest template interceptor for passing Authorization in header

--- kairos-user/src/main/java/com/kairos/util/userContext/UserContextFilter.java
+++ kairos-user/src/main/java/com/kairos/util/userContext/UserContextFilter.java
@@ -1,23 +1,24 @@
 package com.kairos.util.userContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Component
 public class UserContextFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(UserContextFilter.class);
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
 
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        logger.debug("I am entering the user service id with auth token: ", httpServletRequest.getHeader("Authorization"));
+
         UserContext.setCorrelationId(  httpServletRequest.getHeader(UserContext.CORRELATION_ID) );
         UserContext.setUserId( httpServletRequest.getHeader(UserContext.USER_ID) );
         UserContext.setAuthToken( httpServletRequest.getHeader(UserContext.AUTH_TOKEN) );
