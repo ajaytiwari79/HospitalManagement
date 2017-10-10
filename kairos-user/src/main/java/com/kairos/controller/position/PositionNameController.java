8c7401a855ba7fe79671500b3d5afd7e1de63281
vipulpandeykv <vipulp1293@gmail.com>
2017-10-10 18:27:48 +0530

    fixed issus oof getting position of staff and Position name creation at unit level KSP-252

--- kairos-user/src/main/java/com/kairos/controller/position/PositionNameController.java
+++ kairos-user/src/main/java/com/kairos/controller/position/PositionNameController.java
@@ -29,25 +29,25 @@ public class PositionNameController {
 
     @ApiOperation("Create PositionName")
     @PostMapping(value = "/position_name")
-    ResponseEntity<Map<String, Object>> createPositionName( @PathVariable Long unitId, @RequestBody PositionName positionName) {
-        return ResponseHandler.generateResponse(HttpStatus.OK, true, positionNameService.createPositionName( unitId, positionName));
+    ResponseEntity<Map<String, Object>> createPositionName( @RequestParam("type") String type,@PathVariable Long unitId, @RequestBody PositionName positionName) {
+        return ResponseHandler.generateResponse(HttpStatus.OK, true, positionNameService.createPositionName( unitId, positionName,type));
     }
 
     @ApiOperation("Update PositionName")
     @PutMapping(value = "/position_name/{positionNameId}")
-    ResponseEntity<Map<String, Object>> updatePositionName(@PathVariable Long unitId, @PathVariable long positionNameId, @RequestBody PositionName positionName) {
-        return ResponseHandler.generateResponse(HttpStatus.OK, true, positionNameService.updatePositionName(unitId, positionNameId, positionName));
+    ResponseEntity<Map<String, Object>> updatePositionName(@RequestParam("type") String type, @PathVariable Long unitId, @PathVariable long positionNameId, @RequestBody PositionName positionName) {
+        return ResponseHandler.generateResponse(HttpStatus.OK, true, positionNameService.updatePositionName(unitId, positionNameId, positionName,type));
     }
 
     @ApiOperation("Delete PositionName")
     @DeleteMapping(value = "/position_name/{positionNameId}")
-    ResponseEntity<Map<String, Object>> deletePositionName(@PathVariable Long unitId,  @PathVariable Long positionNameId) {
-        return ResponseHandler.generateResponse(HttpStatus.OK, true, positionNameService.deletePositionName(unitId,positionNameId));
+    ResponseEntity<Map<String, Object>> deletePositionName(@RequestParam("type") String type,@PathVariable Long unitId,  @PathVariable Long positionNameId) {
+        return ResponseHandler.generateResponse(HttpStatus.OK, true, positionNameService.deletePositionName(unitId,positionNameId,type));
     }
 
     @ApiOperation("Get PositionName")
     @GetMapping(value = "/position_name/{positionNameId}")
-    ResponseEntity<Map<String, Object>> getPositionName(@PathVariable Long positionNameId) {
+    ResponseEntity<Map<String, Object>> getPositionName(@RequestParam("type") String type,@PathVariable Long positionNameId) {
         return ResponseHandler.generateResponse(HttpStatus.OK, true, positionNameService.getPositionName(positionNameId));
     }
 
