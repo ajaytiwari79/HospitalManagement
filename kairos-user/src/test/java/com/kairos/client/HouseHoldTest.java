fa2bfae4cd27f7f738b1d9e47262cd1f6bb1a661
prabjot singh <prabjot.singh@oodlestechnologies.com>
2017-10-17 17:07:33 +0530

    KP-545 test case to validate house hold

--- kairos-user/src/test/java/com/kairos/client/HouseHoldTest.java
+++ kairos-user/src/test/java/com/kairos/client/HouseHoldTest.java
@@ -3,17 +3,14 @@ package com.kairos.client;
 import com.kairos.UserServiceApplication;
 import com.kairos.persistence.model.user.client.ClientMinimumDTO;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -29,18 +26,22 @@ public class HouseHoldTest {
     @Autowired
     TestRestTemplate restTemplate;
 
+
+    /**
+     * this test checks, client can't add himself in his house hold list
+     */
     @Test
-    public void addHouseHold(){
+    public void validateCitizenAsHouseHold(){
         String baseUrl=getBaseUrl(71L,5753L);
         ClientMinimumDTO clientMinimumDTO = new ClientMinimumDTO("Aage","Bag","2503681059");
         HttpEntity<ClientMinimumDTO> entity = new HttpEntity<>(clientMinimumDTO);
         ResponseEntity<ClientMinimumDTO> response = restTemplate.exchange(
                 baseUrl+"/client/571/household",
                 HttpMethod.POST, entity, ClientMinimumDTO.class);
-       Assert.assertNotNull(response);
+       Assert.assertEquals(409,response.getStatusCodeValue());
     }
 
-    public   final String getBaseUrl(Long organizationId,Long unitId){
+    public final String getBaseUrl(Long organizationId,Long unitId){
         if(organizationId!=null &&unitId!=null ){
             String baseUrl=new StringBuilder(url+"/api/v1/organization/").append(organizationId)
                     .append("/unit/").append(unitId).toString();                    ;
