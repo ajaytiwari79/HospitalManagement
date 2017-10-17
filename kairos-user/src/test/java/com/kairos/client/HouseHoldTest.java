20df84d0583402c59c673c327a113cd7d9dc01e9
prabjot singh <prabjot.singh@oodlestechnologies.com>
2017-10-17 15:51:25 +0530

    KP-545 test case for add client in house hold

--- kairos-user/src/test/java/com/kairos/client/HouseHoldTest.java
+++ kairos-user/src/test/java/com/kairos/client/HouseHoldTest.java
@@ -1,11 +1,20 @@
 package com.kairos.client;
 
 import com.kairos.UserServiceApplication;
+import com.kairos.persistence.model.user.client.ClientMinimumDTO;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -17,12 +26,18 @@ public class HouseHoldTest {
 
     @Value("${server.host.http.url}")
     private String url ;
+    @Autowired
+    TestRestTemplate restTemplate;
 
     @Test
-    @Ignore
     public void addHouseHold(){
-        String baseUrl=getBaseUrl(1L,1L);
-
+        String baseUrl=getBaseUrl(71L,5753L);
+        ClientMinimumDTO clientMinimumDTO = new ClientMinimumDTO("Aage","Bag","2503681059");
+        HttpEntity<ClientMinimumDTO> entity = new HttpEntity<>(clientMinimumDTO);
+        ResponseEntity<ClientMinimumDTO> response = restTemplate.exchange(
+                baseUrl+"/client/571/household",
+                HttpMethod.POST, entity, ClientMinimumDTO.class);
+       Assert.assertNotNull(response);
     }
 
     public   final String getBaseUrl(Long organizationId,Long unitId){
