d9e2eccc1b4fd03c0a9b7da454f9972400f2802a
anilm2 <anil.maurya@oodlestechnologies.com>
2017-10-23 09:01:48 +0530

    enable socket connection via gateway and upgrade protocol for socket connection

--- kairos-gateway/src/main/java/com/kairos/zuulgateway/filter/TrackingFilter.java
+++ kairos-gateway/src/main/java/com/kairos/zuulgateway/filter/TrackingFilter.java
@@ -7,6 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.util.*;
+
 @Component
 public class TrackingFilter extends ZuulFilter{
     private static final Logger logger = LoggerFactory.getLogger(TrackingFilter.class);
@@ -54,7 +58,73 @@ public class TrackingFilter extends ZuulFilter{
         }
 
         RequestContext ctx = RequestContext.getCurrentContext();
-        logger.debug(String.format("Processing incoming request for {}.",  ctx.getRequest().getRequestURI()));
+        RequestWrapper wrapper = new RequestWrapper(ctx.getRequest());
+        String upgradeHeader = wrapper.getHeader("Upgrade");
+        if (null == upgradeHeader) {
+            upgradeHeader = wrapper.getHeader("upgrade");
+            System.out.print("uakdfkdahf");
+        }
+        if (null != upgradeHeader && "websocket".equalsIgnoreCase(upgradeHeader)) {
+            System.out.print("uakdfkdahf upgrade");
+            wrapper.addHeader("connection", "Upgrade");
+            ctx.addZuulRequestHeader("connection", "Upgrade");
+            ctx.setRequest(wrapper);
+        }
+        logger.info(String.format("Processing incoming request for {}.",  ctx.getRequest().getRequestURI()));
         return null;
     }
+
+
+    private static class RequestWrapper extends HttpServletRequestWrapper {
+
+        public RequestWrapper(HttpServletRequest request) {
+            super(request);
+        }
+
+        private Map<String, String> headerMap = new HashMap<String, String>();
+
+        /**
+         * add a header with given name and value
+         *
+         * @param name
+         * @param value
+         */
+        public void addHeader(String name, String value) {
+            headerMap.put(name, value);
+        }
+
+        @Override
+        public String getHeader(String name) {
+            String headerValue = super.getHeader(name);
+            if (headerMap.containsKey(name)) {
+                headerValue = headerMap.get(name);
+            }
+            return headerValue;
+        }
+
+        /**
+         * get the Header names
+         */
+        @Override
+        public Enumeration<String> getHeaderNames() {
+            List<String> names = Collections.list(super.getHeaderNames());
+            for (String name : headerMap.keySet()) {
+                names.add(name);
+            }
+            return Collections.enumeration(names);
+        }
+
+        @Override
+        public Enumeration<String> getHeaders(String name) {
+            List<String> values = Collections.list(super.getHeaders(name));
+
+            if (headerMap.containsKey(name)) {
+                values.add(headerMap.get(name));
+            }
+
+            return Collections.enumeration(values);
+        }
+
+    }
+
 }
