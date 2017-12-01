108644e42032621ac63ff55f01ec1257bd170c98
Prerna Jajoria <prerna@prerna.jajoria>
2017-12-01 17:31:11 +0530

    #KP-1388, Issue fixed in aading household members

--- kairos-user/src/main/java/com/kairos/service/client/ClientService.java
+++ kairos-user/src/main/java/com/kairos/service/client/ClientService.java
@@ -647,6 +647,10 @@ public class ClientService extends UserBaseService {
         Client houseHold = saveDetailsOfHouseHold(minimumDTO);
 
         Long addressIdOfHouseHold = null;
+        Boolean addHouseHoldInCitizenList = false;
+        if(houseHold.getId() == null ){
+            addHouseHoldInCitizenList = true;
+        }
         if(houseHold.getId() != null && houseHold.getHomeAddress() !=null){
             addressIdOfHouseHold = houseHold.getHomeAddress().getId();
         }
@@ -654,12 +658,13 @@ public class ClientService extends UserBaseService {
         saveAddressOfHouseHold(client, houseHold);
         save(houseHold);
 
-        if ( !Optional.ofNullable(houseHold.getId()).isPresent()) {
+        if ( addHouseHoldInCitizenList ) {
             addHouseHoldInOrganization(houseHold, unitId);
         }
 
         // Check and Update Address of all household members
-        if( addressIdOfHouseHold !=null && minimumDTO.getUpdateAddressOfAllHouseholdMembers() && minimumDTO.getUpdateAddressOfAllHouseholdMembers() == true){
+        if( addressIdOfHouseHold !=null && minimumDTO.getUpdateAddressOfAllHouseholdMembers()){
+//            //  && minimumDTO.getUpdateAddressOfAllHouseholdMembers() == true
             clientAddressService.updateAddressOfAllHouseHoldMembers(client.getHomeAddress().getId(), addressIdOfHouseHold);
         }
 
