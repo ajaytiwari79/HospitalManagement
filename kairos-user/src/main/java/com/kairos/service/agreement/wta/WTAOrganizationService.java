4b59b9046342a3122711638125b539190541fc55
pavan kumar <pavan@pavan.kumar>
2018-02-27 16:53:42 +0530

    Removed unused import KP-2333

--- kairos-user/src/main/java/com/kairos/service/agreement/wta/WTAOrganizationService.java
+++ kairos-user/src/main/java/com/kairos/service/agreement/wta/WTAOrganizationService.java
@@ -2,7 +2,6 @@ package com.kairos.service.agreement.wta;
 
 import com.kairos.custom_exception.ActionNotPermittedException;
 import com.kairos.custom_exception.DataNotFoundByIdException;
-import com.kairos.custom_exception.DataNotMatchedException;
 import com.kairos.custom_exception.DuplicateDataException;
 import com.kairos.persistence.model.organization.Organization;
 import com.kairos.persistence.model.user.agreement.cta.RuleTemplate;
