8570b1dc714f8c3c7c5a06ed22198a26d56d814d
vipulpandeykv <vipulp1293@gmail.com>
2018-03-20 11:26:35 +0530

    added keys in dto for request to create expertise KP-2648

--- kairos-user/src/main/java/com/kairos/response/dto/web/experties/CountryExpertiseDTO.java
+++ kairos-user/src/main/java/com/kairos/response/dto/web/experties/CountryExpertiseDTO.java
@@ -1,8 +1,11 @@
 package com.kairos.response.dto.web.experties;
 
+import com.kairos.persistence.model.user.pay_table.FutureDate;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.neo4j.ogm.annotation.typeconversion.DateLong;
 
 import javax.validation.constraints.NotNull;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -12,11 +15,30 @@ public class CountryExpertiseDTO {
 
     private long id;
 
-    @NotEmpty(message = "error.Expertise.name.notEmpty") @NotNull(message = "error.Expertise.name.notnull")
+    @NotEmpty(message = "error.Expertise.name.notEmpty")
+    @NotNull(message = "error.Expertise.name.notnull")
     private String name;
 
     //@NotEmpty(message = "error.Expertise.description.notEmpty") @NotNull(message = "error.Expertise.description.notnull")
     private String description;
+    @NotNull(message = "Start date can't be null")
+    @DateLong
+    @FutureDate
+    private Date startDateMillis;
+
+    @FutureDate
+    @DateLong
+    private Date endDateMillis;
+    @NotNull(message = "Level can not be null")
+    private Long levelId;
+
+    @NotNull(message = "services can not be null")
+    private Long serviceId;
+
+    @NotNull(message = "union can not be null")
+    private Long unionId;
+
+    private int fullTimeWeeklyHours;
 
     private List<Long> tags;
 
