4394daa753b12d438476ad18615f376ae2f9eb8f
Prerna Jajoria <prerna@prerna.jajoria>
2018-04-09 12:04:29 +0530

    #KP-2775, Planning period service added

--- kairosActivity/src/main/java/com/kairos/activity/service/period/PlanningPeriodService.java
+++ kairosActivity/src/main/java/com/kairos/activity/service/period/PlanningPeriodService.java
@@ -1,11 +1,16 @@
 package com.kairos.activity.service.period;
 
+import com.kairos.activity.constants.AppConstants;
+import com.kairos.activity.persistence.model.period.PlanningPeriod;
 import com.kairos.activity.service.MongoBaseService;
 import com.kairos.activity.service.phase.PhaseService;
+import com.kairos.response.dto.web.period.PlanningPeriodDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 /**
  * Created by prerna on 6/4/18.
@@ -14,4 +19,25 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class PlanningPeriodService extends MongoBaseService {
     private static final Logger logger = LoggerFactory.getLogger(PhaseService.class);
+
+    public void createPeriods(Long unitId, PlanningPeriodDTO planningPeriodDTO) {
+
+        PeriodSettings periodSettingsOfParentOrg = null;
+        // Set default values
+        int presenceLimitInYear = AppConstants.PRESENCE_LIMIT_IN_YEAR;
+        int absenceLimitInYear = AppConstants.ABSENCE_LIMIT_IN_YEAR;
+
+        // If parent exists, set settings as of parent
+        if(Optional.ofNullable(parentOrgId).isPresent()){
+            periodSettingsOfParentOrg = periodSettingsMongoRepository.findByUnit(parentOrgId,false);
+        }
+
+        if(Optional.ofNullable(periodSettingsOfParentOrg).isPresent()){
+            presenceLimitInYear = periodSettingsOfParentOrg.getPresenceLimitInYear();
+            absenceLimitInYear = periodSettingsOfParentOrg.getAbsenceLimitInYear();
+        }
+
+        PeriodSettings periodSettings = new PeriodSettings(presenceLimitInYear,absenceLimitInYear, unitId);
+        save(periodSettings);
+    }
 }
