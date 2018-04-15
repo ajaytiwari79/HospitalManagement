72c39eac878d2eb9586bd9e941c54fb5fcc2f5bd
Mohit Shakya <mohit.shakya@oodlestechnologies.com>
2018-04-16 04:20:51 +0530

    removed non-required dto for counters

--- kairosActivity/src/main/java/com/kairos/activity/response/dto/counter/RolewiseCounterGroupingDTO.java
+++
@@ -1,9 +0,0 @@
-package com.kairos.activity.response.dto.counter;
-
-import java.math.BigInteger;
-import java.util.List;
-
-public class RolewiseCounterGroupingDTO {
-    private BigInteger roleId;
-    private List<BigInteger> modulewiseCountersId;
-}
