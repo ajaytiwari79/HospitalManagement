9b0d99b3020ee65822ff45bb3510aac2c3a04f72
pavan kumar <pavan@pavan.kumar>
2018-05-08 16:47:19 +0530

    KP-3260 In-progress

---
+++ kairosActivity/src/main/java/com/kairos/activity/persistence/model/priority_group/PriorityGroup.java
@@ -0,0 +1,7 @@
+package com.kairos.activity.persistence.model.priority_group;
+
+import com.kairos.activity.persistence.model.common.MongoBaseEntity;
+
+public class PriorityGroup extends MongoBaseEntity {
+
+}
