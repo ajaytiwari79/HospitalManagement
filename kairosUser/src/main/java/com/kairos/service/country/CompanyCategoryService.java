58914bb4b02cd1553db566cd29bd5c4477d32b06
harishmodi <harish.modi@oodlestechnologies.com>
2018-05-16 19:23:19 +0530

    KP-3352 "change exception message in property file "

--- kairosUser/src/main/java/com/kairos/service/country/CompanyCategoryService.java
+++ kairosUser/src/main/java/com/kairos/service/country/CompanyCategoryService.java
@@ -11,6 +11,7 @@ import com.kairos.persistence.repository.user.country.CompanyCategoryGraphReposi
 import com.kairos.persistence.repository.user.country.CountryGraphRepository;
 import com.kairos.response.dto.web.company_category.CompanyCategoryDTO;
 import com.kairos.service.UserBaseService;
+import com.kairos.service.exception.ExceptionService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,18 +29,24 @@ public class CompanyCategoryService extends UserBaseService {
     CountryGraphRepository countryGraphRepository;
     @Inject
     CompanyCategoryGraphRepository companyCategoryGraphRepository;
+    @Inject
+    private ExceptionService exceptionService;
+
 
     public CompanyCategoryDTO createCompanyCategory(Long countryId, CompanyCategoryDTO companyCategoryDTO) {
         if (companyCategoryDTO.getName().trim().isEmpty()) {
-            throw new ActionNotPermittedException("Company category Name can't be blank");
+            exceptionService.actionNotPermittedException("error.companyCategory.name.notEmpty");
+
         }
         Optional<Country> country = countryGraphRepository.findById(countryId);
         if (!Optional.ofNullable(country.get()).isPresent()) {
-            throw new DataNotFoundByIdException("Country not found: " + countryId);
+            exceptionService.dataNotFoundByIdException("exception.country.id.notFound",countryId);
+
         }
         boolean isAlreadyExists = companyCategoryGraphRepository.findByCountryAndNameExcludingCurrent(countryId, -1L, "(?i)" + companyCategoryDTO.getName().trim());
         if (isAlreadyExists) {
-            throw new DuplicateDataException("Company category already exists: " + companyCategoryDTO.getName().trim());
+            exceptionService.duplicateDataException("exception.companyCategory.name.alreadyExist", companyCategoryDTO.getName().trim());
+
         }
         CompanyCategory companyCategory = new CompanyCategory(companyCategoryDTO.getName().trim(), companyCategoryDTO.getDescription(), country.get());
         save(companyCategory);
@@ -53,16 +60,19 @@ public class CompanyCategoryService extends UserBaseService {
     public CompanyCategoryResponseDTO updateCompanyCategory(Long countryId, CompanyCategoryDTO companyCategoryDTO) {
 
         if (companyCategoryDTO.getName().trim().isEmpty()) {
-            throw new ActionNotPermittedException("Company category Name can't be blank");
+            exceptionService.actionNotPermittedException("error.companyCategory.name.notEmpty");
+
         }
         CompanyCategory companyCategory = companyCategoryGraphRepository.findByCountryAndCompanycategory(countryId, companyCategoryDTO.getId());
         if (!Optional.ofNullable(companyCategory).isPresent()) {
-            throw new DataNotFoundByIdException("Invalid Company category " + companyCategoryDTO.getId());
+            exceptionService.dataNotFoundByIdException("exception.companyCategory.id.notFound",companyCategoryDTO.getId());
+
         }
         if (!companyCategoryDTO.getName().trim().equalsIgnoreCase(companyCategory.getName())) {
             boolean isAlreadyExists = companyCategoryGraphRepository.findByCountryAndNameExcludingCurrent(countryId, companyCategoryDTO.getId(), "(?i)" + companyCategoryDTO.getName().trim());
             if (isAlreadyExists) {
-                throw new DuplicateDataException("Company category already exists: " + companyCategoryDTO.getName().trim());
+                exceptionService.duplicateDataException("exception.companyCategory.name.alreadyExist", companyCategoryDTO.getName().trim());
+
             }
         }
         companyCategory.setName(companyCategoryDTO.getName().trim());
@@ -74,7 +84,8 @@ public class CompanyCategoryService extends UserBaseService {
     public boolean deleteCompanyCategory(Long countryId, Long companyCategoryId) {
         CompanyCategory companyCategory = companyCategoryGraphRepository.findByCountryAndCompanycategory(countryId, companyCategoryId);
         if (!Optional.ofNullable(companyCategory).isPresent()) {
-            throw new DataNotFoundByIdException("Invalid CompanyCategory " + companyCategoryId);
+            exceptionService.dataNotFoundByIdException("exception.companyCategory.id.notFound",companyCategoryId);
+
         }
         companyCategory.setDeleted(true);
         save(companyCategory);
