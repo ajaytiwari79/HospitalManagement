eeafe7340f249912a38b0019e0942b37e2feec37
Mohit Shakya <mohit.shakya@oodlestechnologies.com>
2018-05-25 12:23:26 +0530

    added work progress over test case issues

--- kairosActivity/src/test/java/com/kairos/activity/config/IntegrationTestConfig.java
+++ kairosActivity/src/test/java/com/kairos/activity/config/IntegrationTestConfig.java
@@ -1,22 +1,26 @@
 package com.kairos.activity.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.kairos.activity.util.userContext.UserContext;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 import java.io.IOException;
+import java.time.LocalDate;
+
 @Configuration
 public class IntegrationTestConfig {
     @Value("${spring.test.authorization}")
@@ -25,13 +29,32 @@ public class IntegrationTestConfig {
     @Bean
     @Primary
     public TestRestTemplate getTestRestTemplate(RestTemplateBuilder restTemplateBuilder) {
-        RestTemplate template =restTemplateBuilder
-                .interceptors(new TestUserContextInterceptor())
-                .build();
-        TestRestTemplate restTemplate = new TestRestTemplate();
+        restTemplateBuilder = restTemplateBuilder
+        .interceptors(new TestUserContextInterceptor())
+        .messageConverters(mappingJackson2HttpMessageConverter());
+        TestRestTemplate restTemplate = new TestRestTemplate(restTemplateBuilder);
         return restTemplate;
     }
 
+    @Bean("objectMapperJackson")
+    @Primary
+    public ObjectMapper serializingObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer());
+        javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(javaTimeModule);
+        return objectMapper;
+    }
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        mappingJackson2HttpMessageConverter.setObjectMapper(serializingObjectMapper());
+        mappingJackson2HttpMessageConverter.setPrettyPrint(true);
+        return mappingJackson2HttpMessageConverter;
+    }
 
     class TestUserContextInterceptor implements ClientHttpRequestInterceptor {
 
