617db426827c7e7689d33e1634b4c63572ee09b6
bobby sharma <bobby@bobby.sharma>
2018-06-01 19:24:33 +0530

    merge request

--- kairos-gdpr/src/main/java/com/kairos/config/mongoEnv_config/EnvConfig.java
+++ kairos-gdpr/src/main/java/com/kairos/config/mongoEnv_config/EnvConfig.java
@@ -23,6 +23,11 @@ public class EnvConfig {
     @Value("${spring.data.mongodb.host}")
     private String mongoHost;
 
+    @Value("${spring.mongodb.username}")
+    private String mongoUserName;
+
+    @Value("${spring.mongodb.password}")
+    private  String mongoPassword;
 
     public String getDataBaseName() {
         return dataBaseName;
@@ -38,5 +43,37 @@ public class EnvConfig {
 
     public int getMongoPort() {
         return mongoPort;
+    }
+
+    public void setDataBaseName(String dataBaseName) {
+        this.dataBaseName = dataBaseName;
+    }
+
+    public void setMongoUri(String mongoUri) {
+        this.mongoUri = mongoUri;
+    }
+
+    public void setMongoPort(int mongoPort) {
+        this.mongoPort = mongoPort;
+    }
+
+    public void setMongoHost(String mongoHost) {
+        this.mongoHost = mongoHost;
+    }
+
+    public String getMongoUserName() {
+        return mongoUserName;
+    }
+
+    public void setMongoUserName(String mongoUserName) {
+        this.mongoUserName = mongoUserName;
+    }
+
+    public String getMongoPassword() {
+        return mongoPassword;
+    }
+
+    public void setMongoPassword(String mongoPassword) {
+        this.mongoPassword = mongoPassword;
     }
 }
