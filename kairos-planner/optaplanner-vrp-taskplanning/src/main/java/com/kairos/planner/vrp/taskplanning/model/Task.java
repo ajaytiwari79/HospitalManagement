726b6432e1b5e7330e4b54f150119e2099a52d7c
pradeep8871 <pradeep@pradeep.singh>
2018-06-08 15:14:42 +0530

    changes for location

--- kairos-planner/optaplanner-vrp-taskplanning/src/main/java/com/kairos/planner/vrp/taskplanning/model/Task.java
+++ kairos-planner/optaplanner-vrp-taskplanning/src/main/java/com/kairos/planner/vrp/taskplanning/model/Task.java
@@ -8,6 +8,7 @@ import org.optaplanner.core.api.domain.variable.InverseRelationShadowVariable;
 import org.optaplanner.core.api.domain.variable.PlanningVariable;
 import org.optaplanner.core.api.domain.variable.PlanningVariableGraphType;
 
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -36,9 +37,10 @@ public class Task{
     private Task prevTask;
     @AnchorShadowVariable(sourceVariableName = "nextTask")
     private Shift shift;
+    private Map<Integer,LocationInfo> locationInfoMap;
 
 
-    public Task(String id,int intallationNo, Double lattitude, Double longitude, Set<String> skills, int duration, String streetName, int houseNo, String block, int floorNo, int post, String city) {
+    public Task(String id,int intallationNo, Double lattitude, Double longitude, Set<String> skills, int duration, String streetName, int houseNo, String block, int floorNo, int post, String city,Map<Integer, LocationInfo> locationInfoMap) {
         this.id = id;
         this.intallationNo = intallationNo;
         this.lattitude = lattitude;
@@ -51,6 +53,16 @@ public class Task{
         this.floorNo = floorNo;
         this.post = post;
         this.city = city;
+        this.locationInfoMap = locationInfoMap;
+    }
+
+
+    public Map<Integer, LocationInfo> getLocationInfoMap() {
+        return locationInfoMap;
+    }
+
+    public void setLocationInfoMap(Map<Integer, LocationInfo> locationInfoMap) {
+        this.locationInfoMap = locationInfoMap;
     }
 
     public String getId() {
--- kairos-planner/src/main/java/com/planner/service/taskService/TaskService.java
+++ kairos-planner/src/main/java/com/planner/service/taskService/TaskService.java
@@ -1,7 +1,9 @@
 package com.planner.service.taskService;
 
+import com.kairos.planner.vrp.taskplanning.model.LocationInfo;
 import com.planner.domain.task.Task;
 import com.planner.repository.taskRepository.TaskRepository;
+import com.planner.service.locationService.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,7 @@ public class TaskService {
 
 
     @Autowired private TaskRepository taskRepository;
+    @Autowired private LocationService locationService;
 
     public void saveTasks(List<Task> taskList){
         taskRepository.saveAll(taskList);
@@ -28,14 +31,17 @@ public class TaskService {
     public List<com.kairos.planner.vrp.taskplanning.model.Task> getUniqueTask(){
         List<Task> taskList = new ArrayList<>(new HashSet(taskRepository.findAll()));
         List<com.kairos.planner.vrp.taskplanning.model.Task> tasks = new ArrayList<>();
+        Map<Integer,Map<Integer,LocationInfo>> locationInfoMap = locationService.getLocationMap();
         Map<Integer,Integer> intallationandDuration = taskList.stream().collect(groupingBy(Task::getIntallationNo,summingInt(Task::getDuration)));
         Map<Integer,Set<String>> intallationandSkill = taskList.stream().collect(groupingBy(Task::getIntallationNo,mapping(Task::getSkill,toSet())));
         Map<Integer,Task> taskMap= taskList.stream().collect(Collectors.toMap(t->t.getIntallationNo(), t->t));
         intallationandDuration.entrySet().forEach(t->{
             Task task = taskMap.get(t.getKey());
-            tasks.add(new com.kairos.planner.vrp.taskplanning.model.Task(task.getId(),task.getIntallationNo(),task.getLattitude(),task.getLongitude(),intallationandSkill.get(task.getIntallationNo()),t.getValue(),task.getStreetName(),task.getHouseNo(),task.getBlock(),task.getFloorNo(),task.getPost(),task.getCity()));
+            tasks.add(new com.kairos.planner.vrp.taskplanning.model.Task(task.getId(),task.getIntallationNo(),task.getLattitude(),task.getLongitude(),intallationandSkill.get(task.getIntallationNo()),t.getValue(),task.getStreetName(),task.getHouseNo(),task.getBlock(),task.getFloorNo(),task.getPost(),task.getCity(),locationInfoMap.get(task.getIntallationNo())));
         });
         return tasks;
     }
+
+
 
 }
