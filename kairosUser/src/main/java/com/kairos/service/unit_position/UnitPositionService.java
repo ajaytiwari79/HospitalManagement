70378c450f3f017fbb90e8ba9fde57849f7b923a
harishmodi <harish.modi@oodlestechnologies.com>
2018-06-19 16:05:11 +0530

    KP-3190 "change in update	unit postion date"

--- kairosUser/src/main/java/com/kairos/service/unit_position/UnitPositionService.java
+++ kairosUser/src/main/java/com/kairos/service/unit_position/UnitPositionService.java
@@ -546,7 +546,7 @@ public class UnitPositionService extends UserBaseService {
 
             }
         }
-        if (unitPositionDTO.getStartLocalDate().isAfter(LocalDate.now())) {
+        if (unitPositionDTO.getStartLocalDate().isBefore(LocalDate.now())||unitPositionDTO.getStartLocalDate().isEqual(LocalDate.now())) {
             exceptionService.actionNotPermittedException("message.startdate.notlessthan.currentdate");
 
         }
