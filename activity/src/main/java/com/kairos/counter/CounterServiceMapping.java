37cde8cb1cf37e62c8c3d13ecff7c47dc4138494
Mohit Shakya <mohit.shakya@oodlestechnologies.com>
2018-08-02 18:29:43 +0530

    reverted conflict resolved.

--- activity/src/main/java/com/kairos/counter/CounterServiceMapping.java
+++ activity/src/main/java/com/kairos/counter/CounterServiceMapping.java
@@ -1,11 +1,12 @@
 package com.kairos.counter;
 
-import com.kairos.activity.counter.enums.CounterType;
+import com.kairos.enums.CounterType;
 import com.kairos.service.counter.CounterService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.Map;
 
