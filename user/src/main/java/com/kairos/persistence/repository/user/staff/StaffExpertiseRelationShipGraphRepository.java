00ccb2ff153aa803914eb7989b394df6c014bcf9
pavan kumar <oodles@pavan.kumar>
2018-08-02 10:47:52 +0530

    Add arrow in Query

--- user/src/main/java/com/kairos/persistence/repository/user/staff/StaffExpertiseRelationShipGraphRepository.java
+++ user/src/main/java/com/kairos/persistence/repository/user/staff/StaffExpertiseRelationShipGraphRepository.java
@@ -42,7 +42,7 @@ public interface StaffExpertiseRelationShipGraphRepository extends Neo4jBaseRepo
 
     @Query("MATCH (staff:Staff) where id(staff) IN {0} " +
             "OPTIONAL MATCH(staff)-[rel:"+STAFF_HAS_EXPERTISE+"]->(expertise:Expertise) " +
-            "OPTIONAL MATCH(staff)-[:"+BELONGS_TO_STAFF+"]->(unitPosition:UnitPosition)-[:"+HAS_EMPLOYMENT_TYPE+"]-(employmentType:EmploymentType) where unitPosition.startDateMillis<={1} AND  (unitPosition.endDateMillis IS NULL or unitPosition.endDateMillis>={1}) " +
+            "OPTIONAL MATCH(staff)-[:"+BELONGS_TO_STAFF+"]->(unitPosition:UnitPosition)-[:"+HAS_EMPLOYMENT_TYPE+"]->(employmentType:EmploymentType) where unitPosition.startDateMillis<={1} AND  (unitPosition.endDateMillis IS NULL or unitPosition.endDateMillis>={1}) " +
             "return id(staff) as staffId,collect(id(expertise)) as expertiseIds,id(employmentType) as employmentTypeId")
     List<StaffExpertiseWrapperQueryResult> getStaffDetailByIds(Set<Long> staffId, Long currentMillis);
 
