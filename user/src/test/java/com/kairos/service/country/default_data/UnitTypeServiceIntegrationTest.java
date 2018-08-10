4c9614cebf35523c0229ae5d205f2b030fd08c27
Vipul Pandey <vipul.pandey@oodlestechnologies.com>
2018-08-10 10:43:46 +0530

    Added test case for unit type

--- user/src/test/java/com/kairos/service/country/default_data/UnitTypeServiceIntegrationTest.java
+++ user/src/test/java/com/kairos/service/country/default_data/UnitTypeServiceIntegrationTest.java
@@ -3,14 +3,17 @@ package com.kairos.service.country.default_data;
 import com.kairos.UserServiceApplication;
 import com.kairos.client.dto.RestTemplateResponseEnvelope;
 import com.kairos.config.OrderTestRunner;
+import com.kairos.persistence.model.country.default_data.UnitTypeQueryResult;
 import com.kairos.service.country.equipment.EquipmentService;
 import com.kairos.service.exception.ExceptionService;
 import com.kairos.user.country.system_setting.UnitTypeDTO;
 import com.kairos.user.reason_code.ReasonCodeDTO;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,25 +25,31 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
 import javax.inject.Inject;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
 @RunWith(OrderTestRunner.class)
 @SpringBootTest(classes = UserServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@ActiveProfiles(profiles = "test")
 public class UnitTypeServiceIntegrationTest {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-
-    static private String url;
+    @Value("${server.host.http.url}")
+    private String url;
     @Inject
     TestRestTemplate restTemplate;
     static Long createdUnitTypeId;
     static Long orgId = 24L;
     static Long countryId = 4L;
-    static  String baseUrlWithCountry;
+    static String baseUrlWithCountry;
 
     @Before
     public void setUp() throws Exception {
@@ -51,7 +60,7 @@ public class UnitTypeServiceIntegrationTest {
     @Test
     @Order(1)
     public void addUnitTypeInCountry() {
-        UnitTypeDTO unitTypeDTO= new UnitTypeDTO("first","first desc");
+        UnitTypeDTO unitTypeDTO = new UnitTypeDTO("first", "first desc");
         HttpEntity<UnitTypeDTO> requestBodyData = new HttpEntity<>(unitTypeDTO);
         ParameterizedTypeReference<RestTemplateResponseEnvelope<UnitTypeDTO>> typeReference =
                 new ParameterizedTypeReference<RestTemplateResponseEnvelope<UnitTypeDTO>>() {
@@ -60,16 +69,40 @@ public class UnitTypeServiceIntegrationTest {
                 baseUrlWithCountry + "/unit_type",
                 HttpMethod.POST, requestBodyData, typeReference);
         Assert.assertTrue(HttpStatus.CREATED.equals(response.getStatusCode()));
+        createdUnitTypeId = response.getBody().getData().getId();
+        logger.info(response.getBody().getData().getId() + "");
     }
 
     @Test
     @Order(2)
     public void getAllUnitTypeOfCountry() {
+        ParameterizedTypeReference<RestTemplateResponseEnvelope<List<UnitTypeQueryResult>>> typeReference =
+                new ParameterizedTypeReference<RestTemplateResponseEnvelope<List<UnitTypeQueryResult>>>() {
+                };
+        ResponseEntity<RestTemplateResponseEnvelope<List<UnitTypeQueryResult>>> response = restTemplate.exchange(
+                baseUrlWithCountry + "/unit_type",
+                HttpMethod.GET, null, typeReference);
+        logger.info(response.toString());
+        Assert.assertTrue(HttpStatus.OK.equals(response.getStatusCode()));
     }
 
     @Test
     @Order(3)
     public void updateUnitTypeOfCountry() {
+        createdUnitTypeId = createdUnitTypeId != null ? createdUnitTypeId : 22172L; // change this accordingly
+        UnitTypeDTO unitTypeDTO = new UnitTypeDTO("firstUpdate", "first desc");
+        unitTypeDTO.setId(createdUnitTypeId);
+        HttpEntity<UnitTypeDTO> requestBodyData = new HttpEntity<>(unitTypeDTO);
+        ParameterizedTypeReference<RestTemplateResponseEnvelope<UnitTypeDTO>> typeReference =
+                new ParameterizedTypeReference<RestTemplateResponseEnvelope<UnitTypeDTO>>() {
+                };
+        ResponseEntity<RestTemplateResponseEnvelope<UnitTypeDTO>> response = restTemplate.exchange(
+                baseUrlWithCountry + "/unit_type/" + createdUnitTypeId,
+                HttpMethod.PUT, requestBodyData, typeReference);
+        Assert.assertTrue(HttpStatus.OK.equals(response.getStatusCode()));
+        createdUnitTypeId = response.getBody().getData().getId();
+        logger.info(response.getBody().getData().getId() + "");
+
     }
 
     public final String getBaseUrl(Long organizationId, Long countryId) {
