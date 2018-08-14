1bbfd28ab61f5036ca21de08362509479db56586
vipulpandeykv <vipul.pandey@oodlestechnologies.com>
2018-08-14 12:50:24 +0530

    revert kmd word from app constants

--- user/src/main/java/com/kairos/constants/AppConstants.java
+++ user/src/main/java/com/kairos/constants/AppConstants.java
@@ -32,15 +32,15 @@ public class AppConstants {
 
     public static final String API_TIME_CARE_SHIFTS = "/api/v1/time_care/getShifts";
     public static final String API_TIME_CARE_ACTIVITIES = "/api/v1/time_care/getWorkPlaces";
-    public static final String API_KMD_CARE_CITIZEN = "/api/v1/kmdNexux/citizen/preferences/{unitId}";
-    public static final String KMD_CARE_CITIZEN_URL = "/api/v1/kmdNexux/citizen/preferences/";
-    public static final String API_KMD_CARE_CITIZEN_GRANTS = "/api/v1/kmdNexux/citizen/grants";
-    public static final String API_KMD_CARE_CITIZEN_RELATIVE_DATA = "/api/v1/kmdNexux/citizen/nextToKin";
+    public static final String API_KMD_CARE_CITIZEN = "/api/v1/kmdNexus/citizen/preferences/{unitId}";
+    public static final String KMD_CARE_CITIZEN_URL = "/api/v1/kmdNexus/citizen/preferences/";
+    public static final String API_KMD_CARE_CITIZEN_GRANTS = "/api/v1/kmdNexus/citizen/grants";
+    public static final String API_KMD_CARE_CITIZEN_RELATIVE_DATA = "/api/v1/kmdNexus/citizen/nextToKin";
     public static final String FORWARD_SLASH = "/";
 
-    public static final String API_KMD_CARE_STAFF_SHIFTS = "/api/v1/kmdNexux/citizen/unit/{unitId}/getShifts/{filterId}";
-    public static final String API_KMD_CARE_TIME_SLOTS = "/api/v1/kmdNexux/citizen/unit/{unitId}/getTimeSlots";
-    public static final String API_KMD_CARE_URL = "/api/v1/kmdNexux/citizen/unit/";
+    public static final String API_KMD_CARE_STAFF_SHIFTS = "/api/v1/kmdNexus/citizen/unit/{unitId}/getShifts/{filterId}";
+    public static final String API_KMD_CARE_TIME_SLOTS = "/api/v1/kmdNexus/citizen/unit/{unitId}/getTimeSlots";
+    public static final String API_KMD_CARE_URL = "/api/v1/kmdNexus/citizen/unit/";
     public static final String API_TIME_SLOTS_NAME = "/api/v1/organization/{organizationId}/unit/{unitId}/time_slot_name";
 
     public static final String API_CREDENTIAL_UPDATE_URL = "/api/v1/user/password";
