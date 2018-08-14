93168996cf2cc736a924a4c3e2a0e4cce73d9dcd
bobby sharma <bobby@bobby.sharma>
2018-08-14 16:05:53 +0530

    GA-411 fixed comment

--- gdpr/src/main/java/com/kairos/enums/AssetAttributeName.java
+++ gdpr/src/main/java/com/kairos/enums/AssetAttributeName.java
@@ -3,8 +3,14 @@ package com.kairos.enums;
 public enum AssetAttributeName {
 
 
-    NAME, DESCRIPTION, HOSTINGLOCATION, MANAGINGDEPARTMENT, ASSETOWNER, STORAGEFORMATS, ORGSECURITYMEASURES, TECHNICALSECURITYMEASURES,
-    PROCESSINGACTIVITY, HOSTINGPROVIDER, HOSTINGTYPE, DATADISPOSAL, ASSETTYPE, ASSETSUBTYPES, DATARETENTIONPERIOD, MINDATASUBJECTVOLUME, MAXDATASUBJECTVOLUME
+    NAME("Name"), DESCRIPTION("Description"), HOSTINGLOCATION("Hosting Location"), MANAGINGDEPARTMENT("Managing Department"), ASSETOWNER("Asset Owner"), STORAGEFORMATS("Storage Format"),
+    ORGSECURITYMEASURES("Organization Security Measures"), TECHNICALSECURITYMEASURES("Technical Security Measure"), PROCESSINGACTIVITIES("Processing Activities"), HOSTINGPROVIDER("Hosting Provider"),
+    HOSTINGTYPE("Hosting Type"), DATADISPOSAL("Data Disposal"), ASSETTYPE("Asset Type"), ASSETSUBTYPES("Sub Asset Type"), MINDATASUBJECTVOLUME("Minimum Data Subject Volume"),
+    MAXDATASUBJECTVOLUME("Max Data Subject Volume"), DATARETENTIONPERIOD("Data Retention Period");
 
+    public String value;
+    AssetAttributeName(String value) {
+        this.value = value;
+    }
 
 }
