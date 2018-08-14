4e044ca16ed6a2aae3222d767b3ab1e695886cce
bobby sharma <bobby@bobby.sharma>
2018-08-14 17:32:59 +0530

    GA-431 resolved bug

--- gdpr/src/main/java/com/kairos/persistance/repository/master_data/processing_activity_masterdata/MasterProcessingActivityRepository.java
+++ gdpr/src/main/java/com/kairos/persistance/repository/master_data/processing_activity_masterdata/MasterProcessingActivityRepository.java
@@ -12,24 +12,21 @@ import java.util.List;
 
 @Repository
 @JaversSpringDataAuditable
-public interface MasterProcessingActivityRepository extends MongoBaseRepository<MasterProcessingActivity,BigInteger>,CustomMasterProcessingActivity {
+public interface MasterProcessingActivityRepository extends MongoBaseRepository<MasterProcessingActivity, BigInteger>, CustomMasterProcessingActivity {
 
 
     @Query("{deleted:false,countryId:?0,organizationId:?1,_id:?2}")
-    MasterProcessingActivity findByIdAndCountryIdAndNonDeleted(Long countryId,Long organizationId,BigInteger id);
+    MasterProcessingActivity findByIdAndCountryIdAndNonDeleted(Long countryId, Long organizationId, BigInteger id);
 
     @Query("{deleted:false,countryId:?0,organizationId:?1}")
-    List<MasterProcessingActivity> getAllMasterProcessingActivity(Long countryId,Long organizationId);
-
-    @Query("{deleted:false,countryId:?0,organizationId:?1,name:{$in:?2}}")
-    List<MasterProcessingActivity>  masterProcessingActivityListByNames(Long countryId,Long organizationId,List<String> names);
+    List<MasterProcessingActivity> getAllMasterProcessingActivity(Long countryId, Long organizationId);
 
     MasterProcessingActivity findByid(BigInteger id);
 
     @Query("{deleted:false,countryId:?0,organizationId:?1,name:?2}")
-    MasterProcessingActivity findByNameAndCountryId(Long countryId,Long organizationId,String name);
+    MasterProcessingActivity findByNameAndCountryId(Long countryId, Long organizationId, String name);
 
     @Query("{deleted:false,countryId:?0,organizationId:?1,_id:{$in:?2},isSubProcess:true}")
-    List<MasterProcessingActivity> getAllMasterSubProcessingActivityByIds(Long countryId,Long organizationId,List<BigInteger> subProcessingActivityIds);
+    List<MasterProcessingActivity> getAllMasterSubProcessingActivityByIds(Long countryId, Long organizationId, List<BigInteger> subProcessingActivityIds);
 
 }
