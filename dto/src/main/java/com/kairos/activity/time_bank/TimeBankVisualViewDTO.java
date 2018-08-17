2ef2173f3e976ce04ff1f0e1a99ccff8cca9c7b5
pradeep8871 <pradeep@pradeep.singh>
2018-08-17 16:15:14 +0530

    add dto to visual view of timebank

---
+++ dto/src/main/java/com/kairos/activity/time_bank/TimeBankVisualViewDTO.java
@@ -0,0 +1,15 @@
+package com.kairos.activity.time_bank;
+
+/**
+ * @author pradeep
+ * @date - 17/8/18
+ */
+
+public class TimeBankVisualViewDTO {
+
+    private long  timeBankMinutes;
+    private long presenceScheduledMinutes;
+    private long absenceScheduledMinutes;
+    private long timeBankChange;
+
+}
