b902854758dbc5a9ff7cd36142127c945ee7014b
pavan kumar <oodles@pavan.kumar>
2018-08-20 19:22:50 +0530

    remove unsed code

--- activity/src/main/java/com/kairos/persistence/model/open_shift/OpenShift.java
+++ activity/src/main/java/com/kairos/persistence/model/open_shift/OpenShift.java
@@ -106,17 +106,4 @@ public class OpenShift extends MongoBaseEntity {
     public void setAssignedStaff(List<Long> assignedStaff) {
         this.assignedStaff = assignedStaff;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        OpenShift openShift = (OpenShift) o;
-        return Objects.equals(id, openShift.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
