e353b61207bcf220e2791cd9f3cce944a86dedc3
bobby sharma <bobby@bobby.sharma>
2018-08-24 12:09:07 +0530

    GA-412 modify Attributes enum

--- gdpr/src/main/java/com/kairos/enums/AssetAttributeName.java
+++ gdpr/src/main/java/com/kairos/enums/AssetAttributeName.java
@@ -3,10 +3,10 @@ package com.kairos.enums;
 public enum AssetAttributeName {
 
 
-    NAME("Name"), DESCRIPTION("Description"), HOSTINGLOCATION("Hosting Location"), MANAGINGDEPARTMENT("Managing Department"), ASSETOWNER("Asset Owner"), STORAGEFORMATS("Storage Format"),
-    ORGSECURITYMEASURES("Organization Security Measures"), TECHNICALSECURITYMEASURES("Technical Security Measure"), PROCESSINGACTIVITIES("Processing Activities"), HOSTINGPROVIDER("Hosting Provider"),
-    HOSTINGTYPE("Hosting Type"), DATADISPOSAL("Data Disposal"), ASSETTYPE("Asset Type"), ASSETSUBTYPES("Sub Asset Type"), MINDATASUBJECTVOLUME("Minimum Data Subject Volume"),
-    MAXDATASUBJECTVOLUME("Max Data Subject Volume"), DATARETENTIONPERIOD("Data Retention Period");
+    NAME("Name"), DESCRIPTION("Description"), HOSTING_LOCATION("Hosting Location"), MANAGING_DEPARTMENT("Managing Department"), ASSET_OWNER("Asset Owner"), STORAGE_FORMAT("Storage Format"),
+    ORGANIZATION_SECURITY_MEASURES("Organization Security Measures"), TECHNICAL_SECURITY_MEASURES("Technical Security Measure"), PROCESSING_ACTIVITIES("Processing Activities"), HOSTING_PROVIDER("Hosting Provider"),
+    HOSTINGTYPE("Hosting Type"), DATADISPOSAL("Data Disposal"), ASSET_TYPE("Asset Type"), ASSET_SUB_TYPE("Sub Asset Type"), MIN_DATA_SUBJECT_VOLUME("Minimum Data Subject Volume"),
+    MAX_DATA_SUBJECT_VOLUME("Max Data Subject Volume"), DATA_RETENTION_PERIOD("Data Retention Period");
 
     public String value;
     AssetAttributeName(String value) {
--- gdpr/src/main/java/com/kairos/enums/ProcessingActivityAttributeName.java
+++ gdpr/src/main/java/com/kairos/enums/ProcessingActivityAttributeName.java
@@ -2,10 +2,10 @@ package com.kairos.enums;
 
 public enum ProcessingActivityAttributeName {
 
-NAME("Name"),DESCRIPTION("Description"),MANAGINGDEPARTMENT("Managing Department"), PROCESSOWNER("Process Owner"),PROCESSINGPURPOSES("Processing Purpose"),DATASOURCES("Data Source"),
-    TRANSFERMETHODS("Transfer method"),ACCESSORPARTIES("Accessor Parties"),PROCESSINGLEGALBASIS("Processing Legal Basis"),RESPONSIBILITYTYPE("Responsibility Type"),CONTROLLERCONTACTINFO("Controller Contact Info")
-    ,DPOCONTACTINFO("DPO Contact Info"),JOINTCONTROLLERCONTACTINFO("Joint Controller Info"),MINDATASUBJECTVOLUME("Minimum Data Subject Volume"),
-    MAXDATASUBJECTVOLUME("Max Data Subject Volume"),DATARETENTIONPERIOD("Data Retention Period");
+NAME("Name"),DESCRIPTION("Description"),MANAGING_DEPARTMENT("Managing Department"), PROCESS_OWNER("Process Owner"),PROCESSING_PURPOSES("Processing Purpose"),DATA_SOURCES("Data Source"),
+    TRANSFERMETHODS("Transfer method"),ACCESSOR_PARTY("Accessor Parties"),PROCESSING_LEGAL_BASIS("Processing Legal Basis"),RESPONSIBILITY_TYPE("Responsibility Type"),CONTROLLER_CONTACT_INFO("Controller Contact Info")
+    ,DPO_CONTACT_INFO("DPO Contact Info"),JOINT_CONTROLLER_CONTACT_INFO("Joint Controller Info"),MIN_DATA_SUBJECT_VOLUME("Minimum Data Subject Volume"),
+    MAX_DATA_SUBJECT_VOLUME("Max Data Subject Volume"),DATA_RETENTION_PERIOD("Data Retention Period");
 
     public String value;
     ProcessingActivityAttributeName(String value) {
