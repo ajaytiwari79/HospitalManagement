bcb7587d11c5ac0d43705fc3a5a29ba2090f0f25
bobby sharma <bobby@bobby.sharma>
2018-08-24 13:08:44 +0530

    GA-412 merge request

--- gdpr/src/main/java/com/kairos/enums/AssetAttributeName.java
+++ gdpr/src/main/java/com/kairos/enums/AssetAttributeName.java
@@ -5,7 +5,7 @@ public enum AssetAttributeName {
 
     NAME("Name"), DESCRIPTION("Description"), HOSTING_LOCATION("Hosting Location"), MANAGING_DEPARTMENT("Managing Department"), ASSET_OWNER("Asset Owner"), STORAGE_FORMAT("Storage Format"),
     ORGANIZATION_SECURITY_MEASURES("Organization Security Measures"), TECHNICAL_SECURITY_MEASURES("Technical Security Measure"), PROCESSING_ACTIVITIES("Processing Activities"), HOSTING_PROVIDER("Hosting Provider"),
-    HOSTINGTYPE("Hosting Type"), DATADISPOSAL("Data Disposal"), ASSET_TYPE("Asset Type"), ASSET_SUB_TYPE("Sub Asset Type"), MIN_DATA_SUBJECT_VOLUME("Minimum Data Subject Volume"),
+    HOSTING_TYPE("Hosting Type"), DATA_DISPOSAL("Data Disposal"), ASSET_TYPE("Asset Type"), ASSET_SUB_TYPE("Sub Asset Type"), MIN_DATA_SUBJECT_VOLUME("Minimum Data Subject Volume"),
     MAX_DATA_SUBJECT_VOLUME("Max Data Subject Volume"), DATA_RETENTION_PERIOD("Data Retention Period");
 
     public String value;
