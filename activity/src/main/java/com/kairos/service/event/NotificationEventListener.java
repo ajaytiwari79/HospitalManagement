15c9b6cd1c171b7a32c1481082cd178c9181048d
yatharthgoviloodles <yatharth.govil@oodlestechnologies.com>
2018-08-27 10:59:17 +0530

    KP-4493 bug fix

--- activity/src/main/java/com/kairos/service/event/NotificationEventListener.java
+++ activity/src/main/java/com/kairos/service/event/NotificationEventListener.java
@@ -26,6 +26,7 @@ public class NotificationEventListener {
     @Autowired
     MailService mailService;
 
+
     @Async
     @EventListener
     public void shiftNotificationEvent(ShiftNotificationEvent shiftNotificationEvent) throws UnsupportedEncodingException {
