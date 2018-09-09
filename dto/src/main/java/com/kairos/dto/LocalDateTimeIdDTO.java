87b477623705a2735afb29997f8d309766b1b318
yatharthgoviloodles <yatharth.govil@oodlestechnologies.com>
2018-09-09 19:02:08 +0530

    switching to another branch

---
+++ dto/src/main/java/com/kairos/dto/LocalDateTimeIdDTO.java
@@ -0,0 +1,19 @@
+package com.kairos.dto;
+import java.time.LocalDateTime;
+import java.math.BigInteger;
+public class LocalDateTimeIdDTO {
+    private LocalDateTime dateTime;
+    private BigInteger id;
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+    public BigInteger getId() {
+        return id;
+    }
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+}
--- scheduler-service/src/main/java/com/kairos/scheduler/controller/SchedulerPanelController.java
+++ scheduler-service/src/main/java/com/kairos/scheduler/controller/SchedulerPanelController.java
@@ -14,7 +14,7 @@ import javax.inject.Inject;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Map;
-
+import java.util.List;
 import static com.kairos.scheduler.constants.ApiConstants.API_SCHEDULER_URL;
 
 @RestController
@@ -44,17 +44,21 @@ public class SchedulerPanelController {
 
     @PostMapping("")
     @ApiOperation("Create Scheduler Panel ")
-    public ResponseEntity<Map<String, Object>> addSchedulerPanel(@RequestParam(value = "integrationConfigurationId", required = false)
-                                                                             BigInteger integrationConfigurationId, @PathVariable  long unitId,
-                                                                 @RequestBody SchedulerPanelDTO schedulerPanelDTO) throws IOException {
-        return ResponseHandler.generateResponse(HttpStatus.OK, true, schedulerPanelService.createSchedulerPanel(unitId, schedulerPanelDTO,
-                integrationConfigurationId));
+    public ResponseEntity<Map<String, Object>> addSchedulerPanel(@PathVariable  long unitId,
+                                                                 @RequestBody List<SchedulerPanelDTO> schedulerPanelDTOs) throws IOException {
+        return ResponseHandler.generateResponse(HttpStatus.OK, true, schedulerPanelService.createSchedulerPanel(unitId, schedulerPanelDTOs));
     }
 
     @PutMapping("/{schedulerPanelId}")
     @ApiOperation("Update Scheduler Panel ")
     public ResponseEntity<Map<String, Object>> updateSchedulerPanel(@RequestBody SchedulerPanelDTO schedulerPanelDTO,@PathVariable BigInteger schedulerPanelId) throws IOException {
         return ResponseHandler.generateResponse(HttpStatus.OK, true, schedulerPanelService.updateSchedulerPanel(schedulerPanelDTO,schedulerPanelId));
+    }
+
+    @PutMapping("/update_date_only")
+    @ApiOperation("Update Scheduler Panel ")
+    public ResponseEntity<Map<String, Object>> updateSchedulerPanelOneTimeTriggerDate(@RequestBody List<LocalDateTimeIdDTO> localDateTimeIdDTOs, @PathVariable Long unitId) throws IOException {
+        return ResponseHandler.generateResponse(HttpStatus.OK, true, schedulerPanelService.updateSchedulerPanelsOneTimeTriggerDate(localDateTimeIdDTOs,unitId));
     }
 
     @GetMapping("/jobDetails/{schedulerPanelId}")
