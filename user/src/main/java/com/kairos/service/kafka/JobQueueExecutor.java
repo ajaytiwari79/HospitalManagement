b6643b3c2f093f5bd951e4ce2be234fc18f095c5
yatharthgoviloodles <yatharth.govil@oodlestechnologies.com>
2018-09-10 13:06:36 +0530

    Revert "Revert "listner added""
    This reverts commit 82402adea7deb24d658de165f9b36059e91eb4b3.

--- user/src/main/java/com/kairos/service/kafka/JobQueueExecutor.java
+++
@@ -1,8 +0,0 @@
-package com.kairos.service.kafka;
-
-import com.kairos.dto.scheduler.KairosSchedulerExecutorDTO;
-
-public interface JobQueueExecutor {
-
-    public void execute(KairosSchedulerExecutorDTO job);
-}
