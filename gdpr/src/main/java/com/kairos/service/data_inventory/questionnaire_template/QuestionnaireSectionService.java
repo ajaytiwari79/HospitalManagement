737ae4dd1e4c863e370d4372f5114d867fc1cf26
bobby sharma <bobby@bobby.sharma>
2018-09-20 17:33:39 +0530

    GA-541 processing activity risk function modification

---
+++ gdpr/src/main/java/com/kairos/service/data_inventory/questionnaire_template/QuestionnaireSectionService.java
@@ -0,0 +1,19 @@
+package com.kairos.service.data_inventory.questionnaire_template;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuestionnaireSectionService {
+
+
+
+
+
+
+
+
+
+
+
+}
