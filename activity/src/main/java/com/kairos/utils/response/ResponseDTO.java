9c516982bbe7a7305cee1f46b45a733fb8a0240f
Mohit Shakya <mohit.shakya@oodlestechnologies.com>
2018-09-25 19:17:33 +0530

    added response dto generic

---
+++ activity/src/main/java/com/kairos/utils/response/ResponseDTO.java
@@ -0,0 +1,47 @@
+package com.kairos.utils.response;
+
+import com.kairos.commons.utils.DateUtils;
+
+public class ResponseDTO<T> {
+    private int status;
+    private boolean success;
+    private T data;
+    private long time_stamp = DateUtils.getCurrentMillis();
+
+    public ResponseDTO() {
+    }
+
+    public ResponseDTO(int status, boolean success, T data){
+        this.status = status;
+        this.success = success;
+        this.data = data;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public long getTime_stamp() {
+        return time_stamp;
+    }
+
+    public void setTime_stamp(long time_stamp) {
+        this.time_stamp = time_stamp;
+    }
+}
--- activity/src/main/java/com/kairos/utils/response/ResponseHandler.java
+++ activity/src/main/java/com/kairos/utils/response/ResponseHandler.java
@@ -1,6 +1,7 @@
 package com.kairos.utils.response;
 
 import com.kairos.commons.utils.DateUtils;
+import org.apache.poi.ss.formula.functions.T;
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +12,12 @@ import java.util.Map;
 /**
  * Created by prabjot on 9/20/16.
  */
-public final class ResponseHandler {
+public final class ResponseHandler<T> {
 
     private ResponseHandler() {
     }
-        public static ResponseEntity<Map<String, Object>> generateResponse(HttpStatus status, boolean isSuccess, Object responseObj) {
+
+    public static ResponseEntity<Map<String, Object>> generateResponse(HttpStatus status, boolean isSuccess, Object responseObj) {
         // Get Time as per UTC format
         long dateTime = DateUtils.getCurrentMillis();
 
@@ -24,8 +26,13 @@ public final class ResponseHandler {
         map.put("isSuccess", isSuccess);
         map.put("data", responseObj);
         map.put("time_stamp", dateTime);
-            return new ResponseEntity<Map<String, Object>>(map, status);
+        return new ResponseEntity<Map<String, Object>>(map, status);
 
+    }
+
+    public static <T> ResponseEntity<ResponseDTO<T>> generateResponseDTO(HttpStatus status, boolean isSuccess, T responsObj){
+        ResponseDTO<T> responseDTO = new ResponseDTO<T>(status.value(), isSuccess, responsObj);
+        return new ResponseEntity<>(responseDTO, status);
     }
 
     public static ResponseEntity<Map<String, Object>> invalidResponse(HttpStatus status, boolean isSuccess, Object errors) {
