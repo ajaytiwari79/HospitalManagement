247a5d983e1641a308ed6a55fe642b5b43d24b96
vipulpandeykv <vipul.pandey@oodlestechnologies.com>
2018-09-25 10:53:33 +0530

    checking issue with org creation KP-4902

--- user/src/main/java/com/kairos/service/auth/UserOauth2Service.java
+++ user/src/main/java/com/kairos/service/auth/UserOauth2Service.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -50,7 +51,7 @@ public class UserOauth2Service implements UserDetailsService {
     }
 
     private List<GrantedAuthority> getPermission(User user){
-       List<GrantedAuthority> permissions = userService.getTabPermission(user.getId());
+       List<GrantedAuthority> permissions = Collections.emptyList();
         return permissions;
     }
 }
