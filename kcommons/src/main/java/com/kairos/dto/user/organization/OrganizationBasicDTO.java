8a7b7a2a108f00370b93e61c3756acda9fe0c60f
pavanoodles <pavan.kumar@oodlestechnologies.com>
2018-10-01 11:05:46 +0530

    fix issue of unit creation by removing trim from desired url

--- kcommons/src/main/java/com/kairos/dto/user/organization/OrganizationBasicDTO.java
+++ kcommons/src/main/java/com/kairos/dto/user/organization/OrganizationBasicDTO.java
@@ -68,7 +68,7 @@ public class OrganizationBasicDTO {
     }
 
     public void setDesiredUrl(String desiredUrl) {
-        this.desiredUrl = desiredUrl.trim();
+        this.desiredUrl = desiredUrl;
     }
 
     public Long getCompanyCategoryId() {
