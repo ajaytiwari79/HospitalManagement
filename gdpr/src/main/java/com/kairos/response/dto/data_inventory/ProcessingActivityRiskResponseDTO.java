d3f6f793fae340a2d482f400810ae8f00a1f0620
bobby sharma <bobby@bobby.sharma>
2018-10-05 16:01:04 +0530

    GA-621 modifiy risk response with procesing activity

--- gdpr/src/main/java/com/kairos/response/dto/data_inventory/ProcessingActivityRiskResponseDTO.java
+++ gdpr/src/main/java/com/kairos/response/dto/data_inventory/ProcessingActivityRiskResponseDTO.java
@@ -2,27 +2,41 @@ package com.kairos.response.dto.data_inventory;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.kairos.response.dto.common.RiskResponseDTO;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProcessingActivityRiskResponseDTO {
-
 
     private BigInteger id;
     private String name;
-    private List<RiskResponseDTO> risks;
-    private List<ProcessingActivityRiskResponseDTO> subProcessingActivities;
+    private Boolean mainParent;
+    private List<RiskResponseDTO> risks=new ArrayList<>();
+    private List<ProcessingActivityRiskResponseDTO> processingActivities;
+
+    public ProcessingActivityRiskResponseDTO(BigInteger id, String name, Boolean mainParent, List<RiskResponseDTO> risks) {
+        this.id = id;
+        this.name = name;
+        this.mainParent = mainParent;
+        this.risks = risks;
+    }
 
     public BigInteger getId() { return id; }
 
     public void setId(BigInteger id) { this.id = id; }
 
-    public List<ProcessingActivityRiskResponseDTO> getSubProcessingActivities() { return subProcessingActivities; }
+    public Boolean getMainParent() { return mainParent; }
 
-    public void setSubProcessingActivities(List<ProcessingActivityRiskResponseDTO> subProcessingActivities) { this.subProcessingActivities = subProcessingActivities; }
+    public void setMainParent(Boolean mainParent) { this.mainParent = mainParent; }
+
+    public List<ProcessingActivityRiskResponseDTO> getProcessingActivities() { return processingActivities; }
+
+    public void setProcessingActivities(List<ProcessingActivityRiskResponseDTO> processingActivities) { this.processingActivities = processingActivities; }
 
     public String getName() { return name; }
 
