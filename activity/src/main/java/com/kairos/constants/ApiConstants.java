63844c82d9241d617c6d6c7fee85188d9c34dc08
Mohit Jain <mohit.jain@oodlestechnologies.com>
2018-10-15 19:35:28 +0530

    KP-generic null url resolved

--- activity/src/main/java/com/kairos/constants/ApiConstants.java
+++ activity/src/main/java/com/kairos/constants/ApiConstants.java
@@ -75,7 +75,7 @@ public final class ApiConstants {
     public static final String TASK_TYPES_SKILLS="/task_type/skills";
     public static final String TIME_SLOTS="/time_slots";
     public static final String DAY_TYPES="/day_types";
-
+    public static final String COUNTRY_COUNTRY_ID=COUNTRY_ID+"/{countryId}";
 
 
 }
