150e82223231f8e9ee79a4db96d9f1da477ab248
vipulpandeykv <vipul.pandey@oodlestechnologies.com>
2018-10-18 18:11:17 +0530

    fixed day duplicate issue of position Line KP-5147

--- user/src/main/java/com/kairos/persistence/repository/user/unit_position/UnitPositionEmploymentTypeRelationShipGraphRepository.java
+++ user/src/main/java/com/kairos/persistence/repository/user/unit_position/UnitPositionEmploymentTypeRelationShipGraphRepository.java
@@ -1,5 +1,6 @@
 package com.kairos.persistence.repository.user.unit_position;
 
+import com.kairos.enums.EmploymentCategory;
 import com.kairos.persistence.model.user.unit_position.UnitPositionEmploymentTypeRelationShip;
 import com.kairos.persistence.repository.custom_repository.Neo4jBaseRepository;
 import org.springframework.data.neo4j.annotation.Query;
@@ -15,4 +16,11 @@ public interface UnitPositionEmploymentTypeRelationShipGraphRepository extends N
     @Query("Match(unitPosition:UnitPosition) where id(unitPosition)={0} " +
             "MATCH(unitPosition)-[relation:" + HAS_EMPLOYMENT_TYPE + "]-(emp:EmploymentType) return emp as employmentType ,relation.employmentTypeCategory as employmentTypeCategory")
     UnitPositionEmploymentTypeRelationShip findEmploymentTypeWithCategoryByUnitPositionId(Long unitPositionId);
+
+    @Query("match(pl:UnitPositionLine),(newEmp:EmploymentType) where id(newEmp)={2} AND id(pl)={0} "+
+    "match(pl)-[old:HAS_EMPLOYMENT_TYPE]-(emp:EmploymentType) "+
+    "detach delete old "+
+    "MERGE(pl)-[new:HAS_EMPLOYMENT_TYPE]->(newEmp) "+
+    "set new.employmentTypeCategory={2} ")
+    void updateEmploymentTypeInCurrentUnitPositionLine(Long positionLineId,Long newEmploymentType,EmploymentCategory newCategory);
 }
