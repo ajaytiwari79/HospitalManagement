20c9d62126aab41c51c6ddde0eab00cea6f3f6b2
vipulpandeykv <vipul.pandey@oodlestechnologies.com>
2018-10-24 18:10:14 +0530

    Added property amountEditableAtUnit at function amount KP-5158

--- user/src/main/java/com/kairos/persistence/repository/user/country/FunctionGraphRepository.java
+++ user/src/main/java/com/kairos/persistence/repository/user/country/FunctionGraphRepository.java
@@ -20,37 +20,38 @@ public interface FunctionGraphRepository extends Neo4jBaseRepository<Function, L
     @Query("MATCH (country:Country)-[:BELONGS_TO]-(function:Function{deleted:false}) where id(country)={0} " +
             "OPTIONAL MATCH(function)-[:HAS_ORGANIZATION_LEVEL]->(level:Level) " +
             "OPTIONAL MATCH(function)-[:HAS_UNION]->(union:Organization{union:true}) " +
-            "with country,function, collect(DISTINCT level) as organizationLevels, collect(DISTINCT union) as unions   return id(function) as id,function.name as name,function.description as description," +
+            "with country,function, collect(DISTINCT level) as organizationLevels, collect(DISTINCT union) as unions   RETURN id(function) as id,function.name as name,function.description as description," +
             "function.startDate as startDate,function.endDate as endDate,unions,organizationLevels,function.icon as icon")
     List<FunctionDTO> findFunctionsByCountry(long countryId);
 
-    @Query("MATCH (country:Country)-[:BELONGS_TO]-(function:Function{deleted:false}) where id(country)={0} return id(function) as id,function.name as name")
+    @Query("MATCH (country:Country)-[:BELONGS_TO]-(function:Function{deleted:false}) where id(country)={0} RETURN id(function) as id,function.name as name")
     List<FunctionDTO> findFunctionsIdAndNameByCountry(long countryId);
 
 
-    @Query("MATCH (c:Country)-[:BELONGS_TO]-(fun:Function{deleted:false}) where id(c)={0} AND LOWER(fun.name)=LOWER({1}) return fun")
+    @Query("MATCH (c:Country)-[:BELONGS_TO]-(fun:Function{deleted:false}) where id(c)={0} AND LOWER(fun.name)=LOWER({1}) RETURN fun")
     Function findByNameIgnoreCase(Long countryId, String name);
 
-    @Query("MATCH (country:Country)-[:BELONGS_TO]-(function:Function{deleted:false}) where id(country)={0} AND id(function) <> {1} AND LOWER(function.name)=LOWER({2}) return function")
+    @Query("MATCH (country:Country)-[:BELONGS_TO]-(function:Function{deleted:false}) where id(country)={0} AND id(function) <> {1} AND LOWER(function.name)=LOWER({2}) RETURN function")
     Function findByNameExcludingCurrent(Long countryId, Long functionId, String name);
 
-    @Query("MATCH(function:Function{deleted:false}) where id(function) IN {0} return function")
+    @Query("MATCH(function:Function{deleted:false}) where id(function) IN {0} RETURN function")
     List<Function> findAllFunctionsById(Set<Long> functionIds);
 
-    @Query("MATCH (level:Level)<-[:" + HAS_ORGANIZATION_LEVEL + "]-(function:Function{deleted:false}) where id(level)={0} return id(function) as id,function.name as name")
+    @Query("MATCH (level:Level)<-[:" + HAS_ORGANIZATION_LEVEL + "]-(function:Function{deleted:false}) where id(level)={0} RETURN id(function) as id,function.name as name")
     List<FunctionDTO> getFunctionsByOrganizationLevel(Long organizationLevelId);
 
-    @Query("match(expertise:Expertise{deleted:false,published:true}) where id(expertise)={0}\n" +
-            "match(expertise)<-[:" + APPLICABLE_FOR_EXPERTISE + "]-(:FunctionalPayment)-[:" + FUNCTIONAL_PAYMENT_MATRIX + "]-(fpm:FunctionalPaymentMatrix) \n" +
-            "match(fpm)-[:" + SENIORITY_LEVEL_FUNCTIONS + "]-(slf:SeniorityLevelFunction) match(slf)-[:" + HAS_FUNCTIONAL_AMOUNT + "]-(fn:Function) \n" +
-            "return distinct id(fn) as id ,fn.name as name")
+    @Query("MATCH(expertise:Expertise{deleted:false,published:true}) where id(expertise)={0}\n" +
+            "MATCH(expertise)<-[:" + APPLICABLE_FOR_EXPERTISE + "]-(:FunctionalPayment)-[:" + FUNCTIONAL_PAYMENT_MATRIX + "]-(fpm:FunctionalPaymentMatrix) \n" +
+            "MATCH(fpm)-[:" + SENIORITY_LEVEL_FUNCTIONS + "]-(slf:SeniorityLevelFunction) " +
+            "MATCH(slf)-[:" + HAS_FUNCTIONAL_AMOUNT + "]-(fn:Function) \n" +
+            "RETURN distinct id(fn) as id ,fn.name as name")
     List<FunctionDTO> getFunctionsByExpertiseId(Long expertiseId);
 
-
-    @Query("match(e:Expertise) where id(e)={0} \n" +
-            "match(e)-[:FOR_SENIORITY_LEVEL]->(sl:SeniorityLevel) where id(sl)={1} \n" +
-            "match(sl)<-[:FOR_SENIORITY_LEVEL]-(slf:SeniorityLevelFunction) \n" +
-            "match(slf)-[:HAS_FUNCTIONAL_AMOUNT]-(function:Function) \n" +
-            "return distinct id(function) as id,function.name as name")
+    @Query("MATCH(expertise:Expertise)-[:"+FOR_SENIORITY_LEVEL+"]->(sl:SeniorityLevel) WHERE id(sl)={1} AND id(expertise)={0} \n" +
+            "match(functionalPayment:FunctionalPayment{deleted:false,hasDraftCopy:false,published:true})-[:" + APPLICABLE_FOR_EXPERTISE +"]->(expertise) where " +
+            " functionalPayment.startDate<=timestamp() AND (functionalPayment.endDate=null OR functionalPayment.endDate>=timestamp())"+
+            "MATCH(sl)<-[:"+FOR_SENIORITY_LEVEL+"]-(slf:SeniorityLevelFunction)-[:"+SENIORITY_LEVEL_FUNCTIONS+"]-(fpm:FunctionalPaymentMatrix)-[:"+FUNCTIONAL_PAYMENT_MATRIX+"]-(functionalPayment) \n" +
+            "MATCH(slf)-[rel:HAS_FUNCTIONAL_AMOUNT]-(function:Function) \n" +
+            "RETURN distinct id(function) as id,function.name as name,rel.amount,function.icon as icon,rel.amountEditableAtUnit as amountEditableAtUnit")
     List<FunctionDTO> getFunctionsByExpertiseAndSeniorityLevel(Long expertiseId,Long seniorityLevelId);
 }
