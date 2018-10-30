6adb4f2bdbc66ccacf2b793441c47bbc97baa57b
pavanoodles <pavan.kumar@oodlestechnologies.com>
2018-10-30 20:49:32 +0530

    Added check

--- activity/src/main/java/com/kairos/controller/payroll_system/PayRollSystemController.java
+++ activity/src/main/java/com/kairos/controller/payroll_system/PayRollSystemController.java
@@ -1,14 +1,13 @@
 package com.kairos.controller.payroll_system;
 
-import com.kairos.dto.activity.payroll_system.PayRollSystemDTO;
 import com.kairos.service.payroll_system.PayRollSystemService;
-import com.kairos.service.payroll_system.country.CountryPayRollSystemService;
-import com.kairos.service.payroll_system.organization.OrganizationPayRollSystemService;
 import com.kairos.utils.response.ResponseHandler;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
 import java.util.Map;
