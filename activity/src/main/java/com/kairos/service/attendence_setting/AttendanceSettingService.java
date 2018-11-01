dc009b6fcf239a8efab73b4c937d8ec773eecebc
pradeep8871 <pradeep@pradeep.singh>
2018-11-01 10:40:23 +0530

    issue resolved in attendence setting

--- activity/src/main/java/com/kairos/service/attendence_setting/AttendanceSettingService.java
+++ activity/src/main/java/com/kairos/service/attendence_setting/AttendanceSettingService.java
@@ -95,9 +95,9 @@ public class AttendanceSettingService extends MongoBaseService {
                     /*if(unitIdAndFlexibleTimeMap.get(checkInshift.getUnitId())==null){
                         exceptionService.dataNotFoundException("error.glidetime.notfound",checkInshift.getUnitId());
                     }*/
-                    ActivityGlideTimeDetails glideTimeDetails = activityMap.get(checkInshift.getActivities().get(0).getActivityName()).getLocationActivityTab().getCheckInGlideTime(LocationEnum.OFFICE);
+                    ActivityGlideTimeDetails glideTimeDetails = activityMap.get(checkInshift.getActivities().get(0).getActivityId()).getLocationActivityTab().getCheckInGlideTime(LocationEnum.OFFICE);
                     if(!Optional.ofNullable(glideTimeDetails).isPresent()){
-                        exceptionService.dataNotFoundException("error.glidetime.notfound",checkInshift.getActivities().get(0).getActivityId());
+                        exceptionService.dataNotFoundException("error.glidetime.notfound",checkInshift.getActivities().get(0).getActivityName());
                     }
                     ZonedDateTime glidStartDateTime = DateUtils.getZonedDateTimeFromZoneId(ZoneId.of(unitIdAndStaffResultMap.get(checkInshift.getUnitId()).getTimeZone())).minusMinutes(glideTimeDetails.getBefore());
                     ZonedDateTime glidEndDateTime = DateUtils.getZonedDateTimeFromZoneId(ZoneId.of(unitIdAndStaffResultMap.get(checkInshift.getUnitId()).getTimeZone())).plusMinutes(glideTimeDetails.getAfter());
