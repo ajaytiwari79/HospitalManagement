50f553a0196642d5dde399a00507ae94fc0411b3
bobby sharma <bobby@bobby.sharma>
2018-11-04 00:23:07 +0530

    GA-603 fixed comment

--- gdpr/src/main/java/com/kairos/service/agreement_template/AgreementSectionService.java
+++ gdpr/src/main/java/com/kairos/service/agreement_template/AgreementSectionService.java
@@ -218,7 +218,7 @@ public class AgreementSectionService extends MongoBaseService {
             saveAgreementSectionAndSubSectionIfClauseNotExist(agreementSectionList, agreementSubSectionListCoresspondingToAgreementSection);
         }
 
-        if (CollectionUtils.isNotEmpty(agreementSectionList)) {
+        if (!CollectionUtils.isEmpty(agreementSectionList)) {
             agreementSectionMongoRepository.saveAll(getNextSequence(agreementSectionList));
         }
         return agreementSectionList.stream().map(AgreementSection::getId).collect(Collectors.toList());
