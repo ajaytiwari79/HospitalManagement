0e665489c89506926819cf80b529f50e40e236c8
pradeep8871 <pradeep@pradeep.singh>
2018-11-05 20:05:04 +0530

    refactor method of veto stop bricks function

--- activity/src/main/java/com/kairos/persistence/model/wta/templates/template_types/VetoAndStopBricksWTATemplate.java
+++ activity/src/main/java/com/kairos/persistence/model/wta/templates/template_types/VetoAndStopBricksWTATemplate.java
@@ -2,11 +2,13 @@ package com.kairos.persistence.model.wta.templates.template_types;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.kairos.commons.utils.DateTimeInterval;
 import com.kairos.commons.utils.DateUtils;
 import com.kairos.dto.activity.shift.WorkTimeAgreementRuleViolation;
 import com.kairos.persistence.model.wta.templates.WTABaseRuleTemplate;
 import com.kairos.wrapper.wta.RuleTemplateSpecificInfo;
 import com.kairos.wrapper.shift.ShiftWithActivityDTO;
+import org.apache.commons.collections.CollectionUtils;
 
 import java.math.BigInteger;
 import java.time.LocalDate;
@@ -29,7 +31,7 @@ public class VetoAndStopBricksWTATemplate extends WTABaseRuleTemplate {
     private LocalDate validationStartDate;
     private BigInteger vetoTypeActivity;
     private BigInteger stopBricksActivity;
-    private List<BigInteger> activities = new ArrayList<>();
+   // private List<BigInteger> activities = new ArrayList<>();
     private float totalBlockingPoints; // It's for a duration from @validationStartDate  till the @numberOfWeeks
 
     public VetoAndStopBricksWTATemplate() {
@@ -85,13 +87,14 @@ public class VetoAndStopBricksWTATemplate extends WTABaseRuleTemplate {
         this.totalBlockingPoints = totalBlockingPoints;
     }
 
-    public List<BigInteger> getActivities() {
+    //This will throw nullPointerException
+    /*public List<BigInteger> getActivities() {
         activities.add(this.stopBricksActivity);
         activities.add(this.vetoTypeActivity);
         return activities;
-    }
+    }*/
 
-    @Override
+   /* @Override
     public void validateRules(RuleTemplateSpecificInfo infoWrapper) {
         if (!isDisabled() && DateUtils.asLocalDate(infoWrapper.getShift().getStartDate()).plusDays(1).isAfter(validationStartDate)) {
 
@@ -113,5 +116,39 @@ public class VetoAndStopBricksWTATemplate extends WTABaseRuleTemplate {
             }
 
         }
+    }*/
+
+    @Override
+    public void validateRules(RuleTemplateSpecificInfo infoWrapper) {
+        if (!isDisabled() && CollectionUtils.containsAny(infoWrapper.getShift().getActivitIds(),getActivityIds())) {
+            DateTimeInterval interval = getIntervalByNumberOfWeeks(infoWrapper.getShift(), numberOfWeeks, validationStartDate);
+            int totalVeto = 0;
+            int totalStopBricks = 0;
+            List<ShiftWithActivityDTO> shifts = new ArrayList<>(infoWrapper.getShifts());
+            shifts.add(infoWrapper.getShift());
+            for (ShiftWithActivityDTO shift : shifts) {
+                if(interval.contains(shift.getStartDate())){
+                    if (shift.getActivitIds().contains(vetoTypeActivity)) {
+                        totalVeto++;
+                    } else if (shift.getActivitIds().contains(stopBricksActivity)) {
+                        totalStopBricks++;
+                    }
+                }
+            }
+            boolean isValid = validateVetoAndStopBrickRules(totalBlockingPoints, totalVeto, totalStopBricks);
+            if (!isValid) {
+                WorkTimeAgreementRuleViolation workTimeAgreementRuleViolation = new WorkTimeAgreementRuleViolation(this.id, this.name, 0, true, false);
+                infoWrapper.getViolatedRules().getWorkTimeAgreements().add(workTimeAgreementRuleViolation);
+            }
+
+        }
     }
+
+    List<BigInteger> getActivityIds(){
+        List<BigInteger> activityIds = new ArrayList<>();
+        CollectionUtils.addIgnoreNull(activityIds,vetoTypeActivity);
+        CollectionUtils.addIgnoreNull(activityIds,stopBricksActivity);
+        return activityIds;
+    }
+
 }
