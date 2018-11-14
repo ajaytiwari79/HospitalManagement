7521f3b6fb77facb318f1fc109e077f447149ac2
pradeep8871 <pradeep@pradeep.singh>
2018-11-14 13:41:08 +0530

    absence type of shift creating issue

--- user/src/main/java/com/kairos/persistence/repository/custom_repository/Neo4jBaseRepositoryImpl.java
+++ user/src/main/java/com/kairos/persistence/repository/custom_repository/Neo4jBaseRepositoryImpl.java
@@ -2,9 +2,12 @@ package com.kairos.persistence.repository.custom_repository;
 
 import org.neo4j.ogm.session.Session;
 import org.springframework.data.neo4j.repository.support.SimpleNeo4jRepository;
+import org.springframework.data.neo4j.util.PagingAndSortingUtils;
 import org.springframework.util.Assert;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 
 public class Neo4jBaseRepositoryImpl<T, ID extends Serializable>
 extends SimpleNeo4jRepository<T, ID> implements Neo4jBaseRepository<T, ID> {
@@ -39,4 +42,7 @@ extends SimpleNeo4jRepository<T, ID> implements Neo4jBaseRepository<T, ID> {
 		Assert.notNull(id, ID_MUST_NOT_BE_NULL);
 		return session.load(clazz, id,depth);
 	}
+
+
+
 }
