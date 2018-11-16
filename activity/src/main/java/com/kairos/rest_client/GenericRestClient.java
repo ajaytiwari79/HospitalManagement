dbc75bb743e0542b7c4bdbb643098d591cf2e87b
pradeep8871 <pradeep@pradeep.singh>
2018-11-16 15:46:14 +0530

    exception message throw changes in user service and actvitiy service

--- activity/src/main/java/com/kairos/rest_client/GenericRestClient.java
+++ activity/src/main/java/com/kairos/rest_client/GenericRestClient.java
@@ -61,6 +61,7 @@ public class GenericRestClient {
     public <T extends Object, V> V publishRequest(T t, Long id, RestClientUrlType restClientUrlType, HttpMethod httpMethod, String uri, List<NameValuePair> queryParam, ParameterizedTypeReference<RestTemplateResponseEnvelope<V>> typeReference, Object... pathParams) {
         final String baseUrl = getUserServiceBaseUrl(restClientUrlType, id) + uri;
         String url = baseUrl +getURIWithParam(queryParam);
+        V responseData = null;
         try {
             ResponseEntity<RestTemplateResponseEnvelope<V>> restExchange =
                     restTemplate.exchange(
@@ -71,13 +72,13 @@ public class GenericRestClient {
             if (!restExchange.getStatusCode().is2xxSuccessful()) {
                 exceptionService.internalError(response.getMessage());
             }
-            return response.getData();
+            responseData = response.getData();
         } catch (HttpClientErrorException e) {
             logger.info("status {}", e.getStatusCode());
             logger.info("response {}", e.getResponseBodyAsString());
             exceptionService.exceptionWithoutConvertInRestClient(ObjectMapperUtils.JsonStringToObject(e.getResponseBodyAsString(),ResponseEnvelope.class).getMessage());
         }
-
+        return responseData;
     }
 
     /**
