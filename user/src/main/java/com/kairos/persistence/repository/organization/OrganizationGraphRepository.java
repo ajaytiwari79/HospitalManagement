837bd6c5432341eec1308b73e5a835912c46ee92
Mohit Jain <mohit.jain@oodlestechnologies.com>
2018-11-19 17:03:04 +0530

    KP-5465 Query Keywards changes with Capital letters

--- user/src/main/java/com/kairos/persistence/repository/organization/OrganizationGraphRepository.java
+++ user/src/main/java/com/kairos/persistence/repository/organization/OrganizationGraphRepository.java
@@ -758,7 +758,7 @@ public interface OrganizationGraphRepository extends Neo4jBaseRepository<Organiz
     void linkUnionSector(Long unionId,Long sectorId);
 
 
-    @Query("Match(parentOrg:Organization{isEnable:true,boardingCompleted: true}) where id(parentOrg)={0}\n" +
+    @Query("Match(parentOrg:Organization{isEnable:true,boardingCompleted: true}) WHERE id(parentOrg)={0}\n" +
             "Match (parentOrg)-[:HAS_SUB_ORGANIZATION*]->(subOrg:Organization{isEnable:true,boardingCompleted: true}) \n" +
             "Optional Match(parentOrganizatinType:OrganizationType{deleted:false})<-[:TYPE_OF]-(parentOrg)-[:SUB_TYPE_OF]->(parentSubOrganizatinType:OrganizationType{deleted:false})\n" +
             "Optional Match(childOrganizatinType:OrganizationType{deleted:false})<-[:TYPE_OF]-(subOrg)-[:SUB_TYPE_OF]->(childSubOrganizatinType:OrganizationType{deleted:false})\n" +
@@ -767,10 +767,10 @@ public interface OrganizationGraphRepository extends Neo4jBaseRepository<Organiz
             "Optional Match(parentAccountType:AccountType{deleted:false})<-[:HAS_ACCOUNT_TYPE]-(parentOrg)\n" +
             "Optional Match(childAccountType:AccountType{deleted:false})<-[:HAS_ACCOUNT_TYPE]-(subOrg)\n" +
             "return \n" +
-            "{organizationType:case when parentOrganizatinType is null then collect(distinct {id:id(childOrganizatinType),name:childOrganizatinType.name})  else collect(distinct {id:id(parentOrganizatinType),name:parentOrganizatinType.name})  end,\n" +
-            "organizationSubType:case when parentSubOrganizatinType is null then collect(distinct {id:id(childSubOrganizatinType),name:childSubOrganizatinType.name})  else collect(distinct {id:id(parentSubOrganizatinType),name:parentSubOrganizatinType.name})  end,\n" +
-            "organizationService:case when parentOrganizatinService is null then collect(distinct {id:id(childOrganizatinService),name:childOrganizatinService.name})  else collect(distinct {id:id(parentOrganizatinType),name:parentOrganizatinType.name})  end,\n" +
-            "organizationSubService:case when parentSubOrganizatinService is null then collect(distinct {id:id(childSubOrganizatinService),name:childSubOrganizatinService.name})  else collect(distinct {id:id(parentSubOrganizatinService),name:parentSubOrganizatinService.name})  end,\n" +
-            "accountType:case when parentAccountType is null then collect(distinct {id:id(childAccountType),name:childAccountType.name})  else collect(distinct {id:id(parentAccountType),name:parentAccountType.name})  end}")
+            "{organizationType:CASE WHEN parentOrganizatinType IS NULL THEN COLLECT(DISTINCT {id:id(childOrganizatinType),name:childOrganizatinType.name})  ELSE COLLECT(DISTINCT {id:id(parentOrganizatinType),name:parentOrganizatinType.name})  END,\n" +
+            "organizationSubType:CASE WHEN parentSubOrganizatinType IS NULL THEN COLLECT(DISTINCT {id:id(childSubOrganizatinType),name:childSubOrganizatinType.name})  ELSE COLLECT(DISTINCT {id:id(parentSubOrganizatinType),name:parentSubOrganizatinType.name})  END,\n" +
+            "organizationService:CASE WHEN parentOrganizatinService IS NULL THEN COLLECT(DISTINCT {id:id(childOrganizatinService),name:childOrganizatinService.name})  ELSE COLLECT(DISTINCT {id:id(parentOrganizatinType),name:parentOrganizatinType.name})  END,\n" +
+            "organizationSubService:CASE WHEN parentSubOrganizatinService IS NULL THEN COLLECT(DISTINCT {id:id(childSubOrganizatinService),name:childSubOrganizatinService.name})  ELSE COLLECT(DISTINCT {id:id(parentSubOrganizatinService),name:parentSubOrganizatinService.name})  END,\n" +
+            "accountType:CASE WHEN parentAccountType IS NULL THEN collect(distinct {id:id(childAccountType),name:childAccountType.name})  ELSE COLLECT(DISTINCT {id:id(parentAccountType),name:parentAccountType.name})  END}")
     Map<String,Object> getFiltersByParentOrganizationId(long parentOrganizationId);
 }
