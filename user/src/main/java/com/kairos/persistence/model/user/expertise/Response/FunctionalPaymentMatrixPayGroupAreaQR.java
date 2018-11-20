333a06d828e8e6ba058c3cf0c7f445d6c3ef5426
vipulpandeykv <vipul.pandey@oodlestechnologies.com>
2018-11-20 13:13:04 +0530

    replaced map to query result in functional payTable KP-5374

--- user/src/main/java/com/kairos/persistence/model/user/expertise/Response/FunctionalPaymentMatrixPayGroupAreaQR.java
+++ user/src/main/java/com/kairos/persistence/model/user/expertise/Response/FunctionalPaymentMatrixPayGroupAreaQR.java
@@ -1,5 +1,6 @@
 package com.kairos.persistence.model.user.expertise.Response;
 
+import com.kairos.persistence.model.user.pay_group_area.PayGroupAreaQueryResult;
 import org.springframework.data.neo4j.annotation.QueryResult;
 
 import java.util.List;
@@ -8,7 +9,7 @@ import java.util.Set;
 
 @QueryResult
 public class FunctionalPaymentMatrixPayGroupAreaQR {
-    private Map<String,Object> payGroupAreas;
+    private List<PayGroupAreaQueryResult> payGroupAreas;
     private List<SeniorityLevelFunctionQR> seniorityLevelFunction;
     private Long id;
 
@@ -16,11 +17,11 @@ public class FunctionalPaymentMatrixPayGroupAreaQR {
         // dc
     }
 
-    public Map<String, Object> getPayGroupAreas() {
+    public List<PayGroupAreaQueryResult> getPayGroupAreas() {
         return payGroupAreas;
     }
 
-    public void setPayGroupAreas(Map<String, Object> payGroupAreas) {
+    public void setPayGroupAreas(List<PayGroupAreaQueryResult> payGroupAreas) {
         this.payGroupAreas = payGroupAreas;
     }
 
