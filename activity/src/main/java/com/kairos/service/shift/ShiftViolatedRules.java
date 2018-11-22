4e48c12627dfa6db964db0fa360e02734282974d
pradeep8871 <pradeep@pradeep.singh>
2018-11-22 20:02:49 +0530

    shift length issue fixed

--- activity/src/main/java/com/kairos/service/shift/ShiftViolatedRules.java
+++ activity/src/main/java/com/kairos/service/shift/ShiftViolatedRules.java
@@ -15,6 +15,7 @@ import java.util.List;
 @Document
 public class ShiftViolatedRules extends MongoBaseEntity{
 
+    //We need proper discussion because i think it should be per phase
     private Shift shift;
     private List<WorkTimeAgreementRuleViolation> workTimeAgreements;
     private List<ActivityRuleViolation> activities;
