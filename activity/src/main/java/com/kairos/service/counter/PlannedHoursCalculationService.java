6e0dcdf677904713f0f60eca0daeab708c27594d
harishmodi <harish.modi@oodlestechnologies.com>
2018-12-11 13:04:30 +0530

    KP-5651 "add filter"

--- activity/src/main/java/com/kairos/service/counter/PlannedHoursCalculationService.java
+++ activity/src/main/java/com/kairos/service/counter/PlannedHoursCalculationService.java
@@ -1,20 +1,75 @@
 package com.kairos.service.counter;
 
+import com.kairos.commons.utils.DateUtils;
+import com.kairos.dto.activity.counter.chart.DataUnit;
 import com.kairos.dto.activity.counter.data.RawRepresentationData;
+import com.kairos.dto.activity.counter.enums.DisplayUnit;
+import com.kairos.dto.activity.counter.enums.RepresentationUnit;
 import com.kairos.enums.FilterType;
 import com.kairos.persistence.model.counter.KPI;
+import com.kairos.persistence.model.time_bank.DailyTimeBankEntry;
+import com.kairos.persistence.repository.time_bank.TimeBankRepository;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+@Service
 public class PlannedHoursCalculationService implements CounterService {
+    @Inject
+    private TimeBankRepository timeBankRepository;
+
+    private Map<Long,Long> calculatePlannedHour(List<Long> staffIds, LocalDate startDate, LocalDate endDate ){
+        List<DailyTimeBankEntry> dailyTimeBankEntries = timeBankRepository.findAllByStaffIdsAndDate(staffIds, DateUtils.asDate(startDate),DateUtils.asDate(endDate));
+        Map<Long,Long> staffPlannedHours = dailyTimeBankEntries.stream().collect(Collectors.groupingBy(DailyTimeBankEntry::getStaffId,Collectors.summingLong(d->d.getTotalTimeBankMin()+d.getContractualMin())));
+        return staffPlannedHours;
+    }
+
+    private List<DataUnit> getPlannedHours(Map<FilterType, List> filterBasedCriteria, boolean kpi){
+        List<Long> staffIds=new ArrayList<>();
+        List<Date> dates=new ArrayList<>();
+        List<Long> unit;
+        List<String> timeType;
+        List<String> approvalStatus;
+        List<String> employmentType;
+        if(kpi && filterBasedCriteria.get(FilterType.SELECTED_STAFF_IDS)!= null){
+            staffIds = filterBasedCriteria.get(FilterType.SELECTED_STAFF_IDS);
+        }else if(filterBasedCriteria.get(FilterType.STAFF_IDS) != null){
+            staffIds = filterBasedCriteria.get(FilterType.STAFF_IDS);
+        }
+        if(filterBasedCriteria.get(FilterType.TIME_INTERVAL) !=null) {
+            dates = filterBasedCriteria.get(FilterType.TIME_INTERVAL);
+        }
+        if(filterBasedCriteria.get(FilterType.UNIT_IDS)!=null) {
+            unit = filterBasedCriteria.get(FilterType.UNIT_IDS);
+        }
+        if(filterBasedCriteria.get(FilterType.TIME_TYPE)!=null) {
+            timeType = filterBasedCriteria.get(FilterType.TIME_TYPE);
+        }
+        if(filterBasedCriteria.get(FilterType.APPROVAL_STATUS)!=null) {
+            approvalStatus = filterBasedCriteria.get(FilterType.APPROVAL_STATUS);
+        }
+        if(filterBasedCriteria.get(FilterType.EMPLOYMENT_TYPE)!=null) {
+            employmentType = filterBasedCriteria.get(FilterType.EMPLOYMENT_TYPE);
+        }
+        return new ArrayList<>();
+    }
+
     @Override
     public RawRepresentationData getCalculatedCounter(Map<FilterType, List> filterBasedCriteria, Long countryId, KPI kpi) {
-        return null;
+        List<DataUnit> dataList=getPlannedHours(filterBasedCriteria,true);
+        return new RawRepresentationData(kpi.getId(), kpi.getTitle(), kpi.getChart(), DisplayUnit.HOURS, RepresentationUnit.DECIMAL, dataList);
     }
 
     @Override
     public RawRepresentationData getCalculatedKPI(Map<FilterType, List> filterBasedCriteria, Long countryId, KPI kpi) {
-        return null;
+        List<DataUnit> dataList=getPlannedHours(filterBasedCriteria,false);
+        return new RawRepresentationData(kpi.getId(), kpi.getTitle(), kpi.getChart(), DisplayUnit.HOURS, RepresentationUnit.DECIMAL, dataList);
     }
 }
