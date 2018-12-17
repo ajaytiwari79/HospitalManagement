dd5fdca6ed3152f7c75acffb49c39a7abf29f636
Mohit Jain <mohit.jain@oodlestechnologies.com>
2018-12-17 12:51:52 +0530

    GA-987 Asset Type criteria added at organization level

--- gdpr/src/main/java/com/kairos/service/questionnaire_template/QuestionnaireTemplateService.java
+++ gdpr/src/main/java/com/kairos/service/questionnaire_template/QuestionnaireTemplateService.java
@@ -326,6 +326,7 @@ public class QuestionnaireTemplateService extends MongoBaseService {
         if (Optional.ofNullable(previousTemplate).isPresent()) {
             exceptionService.duplicateDataException("message.duplicate", "Questionnaire Template", questionnaireTemplateDTO.getName());
         }
+        validateQuestionnaireTemplateDTOByTemplateTypeCriteriaForUnit(unitId,questionnaireTemplateDTO);
         QuestionnaireTemplate questionnaireTemplate = new QuestionnaireTemplate(questionnaireTemplateDTO.getName(), questionnaireTemplateDTO.getDescription(), QuestionnaireTemplateStatus.DRAFT);
         questionnaireTemplate.setOrganizationId(unitId);
         addTemplateTypeToQuestionnaireTemplate(unitId, true, questionnaireTemplate, questionnaireTemplateDTO);
@@ -333,6 +334,58 @@ public class QuestionnaireTemplateService extends MongoBaseService {
         return questionnaireTemplateDTO.setId(questionnaireTemplate.getId());
 
     }
+
+    /**
+     *
+     * @param unitId
+     * @param templateDto
+     */
+    private void validateQuestionnaireTemplateDTOByTemplateTypeCriteriaForUnit(Long unitId, QuestionnaireTemplateDTO templateDto) {
+        QuestionnaireTemplateType questionnaireTemplateType = templateDto.getTemplateType();
+        QuestionnaireTemplate previousMasterTemplate;
+        switch (questionnaireTemplateType) {
+            case ASSET_TYPE:
+                if (!templateDto.isDefaultAssetTemplate()) {
+                    previousMasterTemplate = getQuestionnaireTemplateByAssetTypeOrSubTypeForUnit(unitId, templateDto);
+                } else {
+                    previousMasterTemplate = questionnaireTemplateMongoRepository.findQuestionnaireTemplateByTemplateTypeAndDefaultAssetTemplateAndOrganizationId(unitId, questionnaireTemplateType, true);
+                }
+                break;
+            case RISK:
+                if (QuestionnaireTemplateType.ASSET_TYPE.equals(templateDto.getRiskAssociatedEntity())) {
+                    previousMasterTemplate = getQuestionnaireTemplateByAssetTypeOrSubTypeForUnit(unitId, templateDto);
+                } else {
+                    previousMasterTemplate = questionnaireTemplateMongoRepository.findQuestionnaireTemplateByTemplateTypeAndRiskAssociatedEntityAndOrganizationId(unitId, questionnaireTemplateType, templateDto.getRiskAssociatedEntity());
+                }
+                break;
+            default:
+                previousMasterTemplate = questionnaireTemplateMongoRepository.findQuestionnaireTemplateByCountryIdAndTemplateType(unitId,questionnaireTemplateType);
+                break;
+        }
+        if (previousMasterTemplate != null) {
+            exceptionService.duplicateDataException("message.duplicate.questionnaireTemplate.templateType.config", "Organization Questionnaire template");
+        }
+    }
+
+    /**
+     *
+     * @param unitId
+     * @param templateDto
+     * @return
+     */
+    private QuestionnaireTemplate getQuestionnaireTemplateByAssetTypeOrSubTypeForUnit(Long unitId, QuestionnaireTemplateDTO templateDto) {
+        QuestionnaireTemplate previousMasterTemplate = null;
+        if (templateDto.getAssetSubType() != null) {
+            previousMasterTemplate = questionnaireTemplateMongoRepository.findQuestionnaireTemplateByTemplateTypeAndAssetTypeAndSubAssetTypeByOrganizationId(unitId, templateDto.getAssetType(), Arrays.asList(templateDto.getAssetSubType()),templateDto.getTemplateType());
+        } else {
+            previousMasterTemplate = questionnaireTemplateMongoRepository.findQuestionnaireTemplateByTemplateTypeAndAssetTypeAndByOrganizationId(unitId, templateDto.getAssetType(),templateDto.getTemplateType());
+        }
+        return previousMasterTemplate;
+    }
+
+
+
+
 
     /**
      * @param unitId
