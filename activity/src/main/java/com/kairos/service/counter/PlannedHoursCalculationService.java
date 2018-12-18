814b9f92edf1b28f14b6fb90282191c1264c1903
harishmodi <harish.modi@oodlestechnologies.com>
2018-12-18 10:32:30 +0530

    KP-5748 "change in if query"

--- activity/src/main/java/com/kairos/service/counter/PlannedHoursCalculationService.java
+++ activity/src/main/java/com/kairos/service/counter/PlannedHoursCalculationService.java
@@ -42,13 +42,8 @@ public class PlannedHoursCalculationService implements CounterService {
 //    }
 
     private List<KpiDataUnit> getPlannedHoursKpiData(Long organizationId, Map<FilterType, List> filterBasedCriteria, boolean kpi){
-        List<Long> staffIds=new ArrayList<>();
         Set<BigInteger> timeTypeIds=new HashSet<>();
-        if(kpi && filterBasedCriteria.get(FilterType.SELECTED_STAFF_IDS)!= null){
-            staffIds = getLongValue(filterBasedCriteria.get(FilterType.SELECTED_STAFF_IDS));
-        }else if(filterBasedCriteria.get(FilterType.STAFF_IDS) != null){
-            staffIds = getLongValue(filterBasedCriteria.get(FilterType.STAFF_IDS));
-        }
+        List<Long> staffIds=(filterBasedCriteria.get(FilterType.STAFF_IDS) != null)?getLongValue(filterBasedCriteria.get(FilterType.STAFF_IDS)):new ArrayList<>();
         List<LocalDate> filterDates = (filterBasedCriteria.get(FilterType.TIME_INTERVAL) !=null) ? filterBasedCriteria.get(FilterType.TIME_INTERVAL): Arrays.asList(DateUtils.getStartDateOfWeek(),DateUtils.getEndDateOfWeek());
         List<Long> unitIds = (filterBasedCriteria.get(FilterType.UNIT_IDS)!=null) ? getLongValue(filterBasedCriteria.get(FilterType.UNIT_IDS)):new ArrayList();
         List<String> shiftActivityStatus=(filterBasedCriteria.get(FilterType.APPROVAL_STATUS)!=null)?filterBasedCriteria.get(FilterType.APPROVAL_STATUS):new ArrayList<>();
