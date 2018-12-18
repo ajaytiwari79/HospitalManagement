3484745bd16530cdb04f155b0b6d52ec519d5df8
pavanoodles <pavan.kumar@oodlestechnologies.com>
2018-12-18 17:25:04 +0530

    fixed App constant

--- activity/src/main/java/com/kairos/constants/payroll/PayRollConstants.java
+++ activity/src/main/java/com/kairos/constants/payroll/PayRollConstants.java
@@ -1,11 +1,10 @@
-package com.kairos.constants.payroll;/*
+package com.kairos.constants.payroll;
+/*
  *Created By Pavan on 18/12/18
  *
  */
 
-import com.kairos.constants.AppConstants;
-
-public class PayRollConstants extends AppConstants {
+public class PayRollConstants {
     public static final String PAYROLL="/payroll";
     public static final String DELETE_PAYROLL="/payroll/{payRollId}";
     public static final String UPDATE_PAYROLL="/payroll/{payRollId}";
