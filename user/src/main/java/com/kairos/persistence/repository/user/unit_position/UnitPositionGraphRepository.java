76c77f1811e10c50fde49b55cccc85c7946482f8
pavanoodles <pavan.kumar@oodlestechnologies.com>
2018-12-27 10:46:03 +0530

    Added Fix

--- user/src/main/java/com/kairos/persistence/repository/user/unit_position/UnitPositionGraphRepository.java
+++ user/src/main/java/com/kairos/persistence/repository/user/unit_position/UnitPositionGraphRepository.java
@@ -390,7 +390,7 @@ public interface UnitPositionGraphRepository extends Neo4jBaseRepository<UnitPos
 
     @Query("MATCH(staff:Staff)-[:"+BELONGS_TO+"]->(user:User)  where id(staff)={0}\n" +
             "MATCH(user)<-[:"+BELONGS_TO+"]-(staffList:Staff)\n" +
-            "OPTIONAL MATCH(staffList)-[:"+BELONGS_TO_STAFF+"]->(up:UnitPosition)-[:"+HAS_POSITION_LINES+"]-(positionLine:UnitPositionLine)-[:"+HAS_EMPLOYMENT_TYPE+"]-(empType:EmploymentType) WHERE id(up)<>{3} up.mainUnitPosition=true AND " +
+            "OPTIONAL MATCH(staffList)-[:"+BELONGS_TO_STAFF+"]->(up:UnitPosition{mainUnitPosition:TRUE})-[:"+HAS_POSITION_LINES+"]-(positionLine:UnitPositionLine)-[:"+HAS_EMPLOYMENT_TYPE+"]-(empType:EmploymentType) WHERE id(up)<> {3} AND  " +
             "(({2} IS NULL AND (up.endDate IS NULL OR DATE(up.endDate) >= DATE({1}))) \n" +
             "OR ({2} IS NOT NULL AND DATE(up.startDate) <= DATE({2}) AND (up.endDate IS NULL OR DATE(up.endDate)>DATE({1}))) ) \n" +
             "WITH up,collect(empType) as empType  \n" +
