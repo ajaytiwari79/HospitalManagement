41c4473d8aa5dabc62f38abaf9508b34ddcfd214
pradeep8871 <pradeep@pradeep.singh>
2018-12-28 15:42:55 +0530

    new util method added

--- kcommons/src/main/java/com/kairos/commons/utils/ObjectUtils.java
+++ kcommons/src/main/java/com/kairos/commons/utils/ObjectUtils.java
@@ -1,6 +1,12 @@
 package com.kairos.commons.utils;
 
+import org.springframework.lang.Nullable;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -19,5 +25,19 @@ public class ObjectUtils {
     {
         Map<Object, Boolean> map = new ConcurrentHashMap<>();
         return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+
+    public static boolean isCollectionEmpty(@Nullable Collection<?> collection){
+        return (collection == null || collection.isEmpty());
+    }
+    public static boolean isEmpty(@Nullable Map<?, ?> map) {
+        return (map == null || map.isEmpty());
+    }
+    public static boolean isNotEmpty(@Nullable Map<?, ?> map) {
+        return !isEmpty(map);
+    }
+
+    public static <T> boolean isNull(T object){
+        return !Optional.ofNullable(object).isPresent();
     }
 }
