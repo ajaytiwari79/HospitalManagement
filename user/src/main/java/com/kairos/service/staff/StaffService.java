fdb020cb6597fbac7429d616e1feb71f2cca0eb3
harishmodi <harish.modi@oodlestechnologies.com>
2019-01-09 17:39:52 +0530

    KP-5281 "change in staffsevice"

--- user/src/main/java/com/kairos/service/staff/StaffService.java
+++ user/src/main/java/com/kairos/service/staff/StaffService.java
@@ -987,8 +987,6 @@ public class StaffService {
         }
         employment.getUnitPermissions().add(unitPermission);
         employmentGraphRepository.save(employment);
-        activityIntegrationService.createDefaultKPISettingForStaff(new DefaultKPISettingDTO(Arrays.asList(employment.getStaff().getId())), organization.getId());
-
     }
 
     public void setUnitManagerAndEmployment(Organization organization, User user, Long accessGroupId) {
