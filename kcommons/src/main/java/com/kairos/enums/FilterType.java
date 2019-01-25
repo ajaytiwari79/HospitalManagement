8f14e5de8c553dda6bf1c44e532eb1cc86ab7c9b
harishmodi <harish.modi@oodlestechnologies.com>
2019-01-25 11:35:36 +0530

    KP-6039 "fixed change"

--- kcommons/src/main/java/com/kairos/enums/FilterType.java
+++ kcommons/src/main/java/com/kairos/enums/FilterType.java
@@ -10,7 +10,7 @@ public enum FilterType {
     EMPLOYMENT_TYPE("Employment Type"), EXPERTISE("Expertise"), STAFF_STATUS("Status"), GENDER("Gender"), ENGINEER_TYPE("Engineer Type"),
     TIME_TYPE("Time Type"), PLANNED_TIME_TYPE("Planned Time Type"), ACTIVITY_CATEGORY_TYPE("Category Type"), ORGANIZATION_TYPE("Organization Type"),
     STAFF_IDS("Staff Ids"), ACTIVITY_IDS("Activity"), UNIT_IDS("Unit"), TIME_INTERVAL("Time Interval"),UNIT_POSITION("Unit Position"), SELECTED_STAFF_IDS("Selected Staff IDs"),
-    APPROVAL_STATUS("Approval Status"),PHASE("phase"),DAYS_OF_WEEK("days of week"),DAY_TYPE("day type"),TIME_SLOT("time slot");
+    ACTIVITY_STATUS("Activity Status"),PHASE("phase"),DAYS_OF_WEEK("days of week"),DAY_TYPE("day type"),TIME_SLOT("time slot");
 
     public String value;
 
