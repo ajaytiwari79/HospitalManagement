77d19728b7ea0aee97e056a4dfc2efc78db2abcd
harishmodi <harish.modi@oodlestechnologies.com>
2019-01-29 12:45:13 +0530

    KP-6174 "change if statement"

--- activity/src/main/java/com/kairos/service/wta/WTAService.java
+++ activity/src/main/java/com/kairos/service/wta/WTAService.java
@@ -229,8 +229,10 @@ public class WTAService extends MongoBaseService {
 
 
     public WTAResponseDTO updateWtaOfCountry(Long countryId, BigInteger wtaId, WTADTO updateDTO) {
-        if (updateDTO.getStartDate().isAfter(getLocalDate())) {
-            exceptionService.actionNotPermittedException("message.wta.start-current-date");
+        if(isNotNull(updateDTO.getEndDate())) {
+            if (updateDTO.getStartDate().isAfter(updateDTO.getEndDate())) {
+                exceptionService.actionNotPermittedException("message.wta.start-end-date");
+            }
         }
         WorkingTimeAgreement workingTimeAgreement = wtaRepository.getWtaByNameExcludingCurrent(updateDTO.getName(), countryId, wtaId, updateDTO.getOrganizationType(), updateDTO.getOrganizationSubType());
         if (Optional.ofNullable(workingTimeAgreement).isPresent()) {
