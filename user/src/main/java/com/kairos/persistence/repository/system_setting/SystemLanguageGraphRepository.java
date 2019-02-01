a33825add69dd5b5acdc51d063ad9226e6fdec2a
harishmodi <harish.modi@oodlestechnologies.com>
2019-02-01 17:39:45 +0530

    Added changes of defaul language

--- user/src/main/java/com/kairos/persistence/repository/system_setting/SystemLanguageGraphRepository.java
+++ user/src/main/java/com/kairos/persistence/repository/system_setting/SystemLanguageGraphRepository.java
@@ -12,49 +12,49 @@ import static com.kairos.persistence.model.constants.RelationshipConstants.*;
 @Repository
 public interface SystemLanguageGraphRepository extends Neo4jBaseRepository<SystemLanguage,Long> {
 
-    @Query("Match (language:SystemLanguage{deleted:false}) WHERE lower(language.name)=lower({0})\n" +
+    @Query("MATCH (language:SystemLanguage{deleted:false}) WHERE lower(language.name)=lower({0})\n" +
             "RETURN CASE WHEN count(language)>0 THEN true ELSE false END")
     boolean isSystemLanguageExistsWithSameName(String name);
 
-    @Query("Match (language:SystemLanguage{deleted:false}) RETURN language")
+    @Query("MATCH (language:SystemLanguage{deleted:false}) RETURN language")
     List<SystemLanguage> getListOfSystemLanguage();
 
 
-    @Query("Match (language:SystemLanguage{deleted:false,active:true})  return language")
+    @Query("MATCH (language:SystemLanguage{deleted:false,active:true})  RETURN language")
     List<SystemLanguage> getActiveSystemLanguages();
 
-    @Query("Match (language:SystemLanguage{defaultLanguage:true,deleted:false})\n" +
+    @Query("MATCH (language:SystemLanguage{defaultLanguage:true,deleted:false})\n" +
             "RETURN CASE WHEN count(language)>0 THEN true ELSE false END")
     boolean isDefaultSystemLanguageExists();
 
-    @Query("Match (language:SystemLanguage{defaultLanguage:true,deleted:false, active:true}) WHERE NOT(id(language)={0}) \n" +
+    @Query("MATCH (language:SystemLanguage{defaultLanguage:true,deleted:false, active:true}) WHERE NOT(id(language)={0}) \n" +
             "RETURN CASE WHEN count(language)>0 THEN true ELSE false END")
     boolean isDefaultSystemLanguageExistsExceptId(Long syatemLanguageId);
 
-    @Query("Match (c:Country)-[:"+ HAS_SYSTEM_LANGUAGE+"{defaultLanguage:true}]-(language:SystemLanguage) WHERE id(language)={0} \n" +
+    @Query("MATCH (c:Country)-[:"+ HAS_SYSTEM_LANGUAGE+"{defaultLanguage:true}]-(language:SystemLanguage) WHERE id(language)={0} \n" +
             "RETURN CASE WHEN count(language)>0 THEN true ELSE false END")
     boolean isSystemLanguageSetInAnyCountry(Long syatemLanguageId);
 
-    @Query("Match (c:Country)-[:"+ HAS_SYSTEM_LANGUAGE +"]-(language:SystemLanguage) WHERE id(c)={0} return language")
+    @Query("MATCH (c:Country)-[:"+ HAS_SYSTEM_LANGUAGE +"]-(language:SystemLanguage) WHERE id(c)={0} RETURN language")
     SystemLanguage getSystemLanguageOfCountry(Long countryId);
 
 
-    @Query("Match (language:SystemLanguage{deleted:false}) WHERE lower(language.name)=lower({0})\n" +
+    @Query("MATCH (language:SystemLanguage{deleted:false}) WHERE lower(language.name)=lower({0})\n" +
             "RETURN language")
     SystemLanguage findSystemLanguageByName(String name);
 
-    @Query("Match (language:SystemLanguage{deleted:false}) SET language.defaultLanguage={0}")
+    @Query("MATCH (language:SystemLanguage{deleted:false}) SET language.defaultLanguage={0}")
     void setDefaultStatusForAllLanguage(boolean defaultStatus);
 
-    @Query("Match (language:SystemLanguage{deleted:false, defaultLanguage:true}) return language")
+    @Query("MATCH (language:SystemLanguage{deleted:false, defaultLanguage:true}) RETURN language")
     SystemLanguage getDefaultSystemLangugae();
 
-    @Query("Match (language:SystemLanguage{deleted:false}) WHERE id(language)={0} RETURN language")
+    @Query("MATCH (language:SystemLanguage{deleted:false}) WHERE id(language)={0} RETURN language")
     SystemLanguage findSystemLanguageById(Long id);
 
 
-    @Query("Match (c:Country)-[rel:"+ HAS_SYSTEM_LANGUAGE +"]->(language:SystemLanguage{active:true}) WHERE id(c)={0} " +
-            "return id(language) as id,language.name as name,language.active as active,rel.defaultLanguage as defaultLanguage")
+    @Query("MATCH (c:Country)-[rel:"+ HAS_SYSTEM_LANGUAGE +"]->(language:SystemLanguage{active:true}) WHERE id(c)={0} " +
+            "RETURN id(language) as id,language.name as name,language.active as active,rel.defaultLanguage as defaultLanguage")
     List<SystemLanguageQueryResult> findSystemLanguagesByCountryId(Long countryId);
 
 }
