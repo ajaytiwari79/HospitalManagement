0980da613a0dd0ad3f2f909ef4d05bea0b2cd96b
pavanoodles <pavan.kumar@oodlestechnologies.com>
2019-02-19 11:07:06 +0530

    Added fix of access grouo

--- user/src/main/java/com/kairos/service/access_permisson/AccessGroupService.java
+++ user/src/main/java/com/kairos/service/access_permisson/AccessGroupService.java
@@ -280,7 +280,7 @@ public class AccessGroupService {
         if (!unit.isParentOrganization()) {
             unit = organizationGraphRepository.getParentOfOrganization(unit.getId());
         }
-        return accessGroupRepository.getOrganizationAccessGroupByRole(unit.getId(), AccessGroupRole.MANAGEMENT.toString());
+        return accessGroupRepository.getAccessGroupsForUnit(unit.getId());
     }
 
     public List<AccessGroup> getAccessGroups(long organizationId) {
