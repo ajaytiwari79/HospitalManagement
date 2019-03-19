55c9ce0dddaad151f813fdb0321ab9e1b8694517
bobby sharma <bobby@bobby.sharma>
2019-03-19 16:33:33 +0530

    replace list with set

--- kcommons/src/main/java/com/kairos/dto/gdpr/data_inventory/AssetTypeOrganizationLevelDTO.java
+++ kcommons/src/main/java/com/kairos/dto/gdpr/data_inventory/AssetTypeOrganizationLevelDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AssetTypeOrganizationLevelDTO {
@@ -19,7 +21,7 @@ public class AssetTypeOrganizationLevelDTO {
     @Pattern(message = "error.message.number.and.special.character.notAllowed", regexp = "^[a-zA-Z\\s]+$")
     private String name;
     @Valid
-    private List<OrganizationLevelRiskDTO> risks = new ArrayList<>();
+    private Set<OrganizationLevelRiskDTO> risks = new HashSet<>();
     @Valid
     private List<AssetTypeOrganizationLevelDTO> subAssetTypes = new ArrayList<>();
 
@@ -32,9 +34,9 @@ public class AssetTypeOrganizationLevelDTO {
 
     public void setName(String name) { this.name = name; }
 
-    public List<OrganizationLevelRiskDTO> getRisks() { return risks; }
+    public Set<OrganizationLevelRiskDTO> getRisks() { return risks; }
 
-    public void setRisks(List<OrganizationLevelRiskDTO> risks) { this.risks = risks; }
+    public void setRisks(Set<OrganizationLevelRiskDTO> risks) { this.risks = risks; }
 
     public List<AssetTypeOrganizationLevelDTO> getSubAssetTypes() { return subAssetTypes; }
 
