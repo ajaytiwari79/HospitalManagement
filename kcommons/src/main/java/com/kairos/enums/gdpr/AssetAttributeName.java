2f3a9eda53da2f093785979b25ed6418d1391cf7
bobby sharma <bobby@bobby.sharma>
2019-04-09 18:35:51 +0530

    KP-6870 questionnaire template fix

--- kcommons/src/main/java/com/kairos/enums/gdpr/AssetAttributeName.java
+++ kcommons/src/main/java/com/kairos/enums/gdpr/AssetAttributeName.java
@@ -5,7 +5,7 @@ public enum AssetAttributeName {
 
     NAME("name"), DESCRIPTION("description"), HOSTING_LOCATION("hostingLocation"), STORAGE_FORMAT("storageFormats"),
     ORGANIZATION_SECURITY_MEASURES("orgSecurityMeasures"), TECHNICAL_SECURITY_MEASURES("technicalSecurityMeasures"), HOSTING_PROVIDER("hostingProvider"),
-    HOSTING_TYPE("hostingType"), DATA_DISPOSAL("dataDisposal"), ASSET_TYPE("assetType"), ASSET_SUB_TYPE("subAssetTypes"),  DATA_RETENTION_PERIOD("dataRetentionPeriod");
+    HOSTING_TYPE("hostingType"), DATA_DISPOSAL("dataDisposal"), ASSET_TYPE("assetType"), ASSET_SUB_TYPE("subAssetType"),  DATA_RETENTION_PERIOD("dataRetentionPeriod");
 
     public String value;
     AssetAttributeName(String value) {
