ecbdd9ada8b128676d0612721842ca692b731d12
pradeep <pradeep.singh@oodlestechnologies.com>
2019-04-16 10:03:14 +0530

    solver config functionality

---
+++ planner/src/main/java/com/planner/constants/ActivityMongoConstant.java
@@ -0,0 +1,16 @@
+package com.planner.constants;
+
+/**
+ * pradeep
+ * 15/4/19
+ */
+public class ActivityMongoConstant {
+
+    public static final String DB_NAME = "spring.data.activity.mongodb.database";
+
+    public static final String DB_URL = "spring.data.activity.mongodb.host";
+
+    public static final String DB_PORT = "spring.data.activity.mongodb.port";
+
+    public static final String MONGO_URI = "spring.data.activity.mongodb.uri";
+}
