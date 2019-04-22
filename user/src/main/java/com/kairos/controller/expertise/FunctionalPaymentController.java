03068fa20c727fe82e9bb4ede33a621f5a0dfd8f
pavanoodles <pavan.kumar@oodlestechnologies.com>
2019-04-22 13:07:56 +0530

    Added functionaliy of visualizing both table pulished and draft at same time

---
+++ user/src/main/java/com/kairos/controller/expertise/FunctionalPaymentController.java
@@ -0,0 +1,12 @@
+package com.kairos.controller.expertise;
+/*
+ *Created By Pavan on 22/4/19
+ *
+ */
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunctionalPaymentController {
+
+}
