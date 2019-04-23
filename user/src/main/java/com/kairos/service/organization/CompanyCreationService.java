941f9d95c6e61af13bc9c193f8d492f532520a45
harshitverma21 <harshitverma21@gmail.com>
2019-04-23 18:38:25 +0530

    added the check for is suername updated

--- user/src/main/java/com/kairos/service/organization/CompanyCreationService.java
+++ user/src/main/java/com/kairos/service/organization/CompanyCreationService.java
@@ -301,6 +301,7 @@ public class CompanyCreationService {
                 user.setFirstName(unitManagerDTO.getFirstName());
                 user.setLastName(unitManagerDTO.getLastName());
                 user.setUserName(unitManagerDTO.getUserName());
+                user.setUserNameUpdated(true);
                 userGraphRepository.save(user);
             } else {
                 if(unitManagerDTO.getCprNumber() != null) {
@@ -322,12 +323,14 @@ public class CompanyCreationService {
                 if(anotherUserExistBySameEmailOrCPR != 0) {
                     exceptionService.duplicateDataException("message.cprNumberEmail.notNull");
                 }
+
                 user.setEmail(unitManagerDTO.getEmail());
                 user.setUserName(unitManagerDTO.getUserName());
                 user.setCprNumber(unitManagerDTO.getCprNumber());
                 user.setFirstName(unitManagerDTO.getFirstName());
                 user.setLastName(unitManagerDTO.getLastName());
                 setEncryptedPasswordAndAge(unitManagerDTO, user);
+                user.setUserNameUpdated(true);
                 userGraphRepository.save(user);
                 if(unitManagerDTO.getAccessGroupId() != null) {
                     setAccessGroupInUserAccount(user, organization.getId(), unitManagerDTO.getAccessGroupId(), union);
@@ -344,7 +347,8 @@ public class CompanyCreationService {
                         setAccessGroupInUserAccount(user, organization.getId(), unitManagerDTO.getAccessGroupId(), union);
                     } else {
                         user = new User(unitManagerDTO.getCprNumber(), unitManagerDTO.getFirstName(),
-                                unitManagerDTO.getLastName(), unitManagerDTO.getEmail(), unitManagerDTO.getUserName());
+                                unitManagerDTO.getLastName(), unitManagerDTO.getEmail(), unitManagerDTO.getUserName()
+                                ,true);
                         setEncryptedPasswordAndAge(unitManagerDTO, user);
                     }
                     userGraphRepository.save(user);
