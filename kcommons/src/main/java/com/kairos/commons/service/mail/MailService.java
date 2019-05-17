d6fde4e2e9e6680fd66ca401231119a665f4510c
pradeep <pradeep.singh@oodlestechnologies.com>
2019-05-17 11:11:13 +0530

    redis issue fixed on local

--- kcommons/src/main/java/com/kairos/commons/service/mail/MailService.java
+++ kcommons/src/main/java/com/kairos/commons/service/mail/MailService.java
@@ -167,7 +167,7 @@ public class MailService {
     //Todo Please don't use this method for sending any Custom exception
     public void sendMailToBackendOnException(Exception ex){
        //TODO commented below as we using free account for of send grid which limits 100 emails per day
-        /* if(envConfigCommon.getCurrentProfile().equals(PRODUCTION) || envConfigCommon.getCurrentProfile().equals(QA)){
+        /* if(envConfigCommon.getCurrentProfile().equals(PRODUCTION_PROFILE) || envConfigCommon.getCurrentProfile().equals(QA_PROFILE)){
             StringBuffer body = new StringBuffer("");
             for (StackTraceElement stackTraceElement : ex.getStackTrace()) {
                 if(stackTraceElement.getClassName().contains(PACKAGE_NAME)) {
