b532b463c1751e30581c89a1aea3dbf13c0e1663
pavanoodles <pavan.kumar@oodlestechnologies.com>
2019-05-22 10:04:48 +0530

    Added management as team leaderr

--- user/src/main/java/com/kairos/constants/UserMessagesConstants.java
+++ user/src/main/java/com/kairos/constants/UserMessagesConstants.java
@@ -267,6 +267,7 @@ public class UserMessagesConstants {
     public static final String ERROR_TEAMSERVICE_STAFFORTEAM_NOTEMPTY = "error.teamservice.stafforteam.notEmpty";
     public static final String MESSAGE_TEAMSERVICE_SOMETHINGWRONG = "message.teamservice.somethingwrong";
     public static final String MESSAGE_TEAMSERVICE_TEAM_NOTFOUND = "message.teamservice.team.notFound";
+    public static final String STAFF_CAN_NOT_BE_TEAM_LEADER="message.staff.team.leader";
     public static final String MESSAGE_TEAMSERVICE_TEAM_ALREADYEXISTS_IN_UNIT = "message.teamservice.team.alreadyexists.in.unit";
     public static final String MESSAGE_TIMESLOT_ID_NOTFOUND = "message.timeslot.id.notfound";
     public static final String TIMESLOT_NOT_FOUND  = "timeslot.not.found ";
