9596d69b77db4bd6a09b44c99600719d5f9c2f20
shiv kumar <shiv@shiv.kumar>
2019-05-24 12:59:29 +0530

    KP-7264 : Fixed

--- gdpr/src/main/java/com/kairos/service/questionnaire_template/QuestionnaireSectionService.java
+++ gdpr/src/main/java/com/kairos/service/questionnaire_template/QuestionnaireSectionService.java
@@ -117,6 +117,7 @@ public class QuestionnaireSectionService {
                     question = isOrganization ? new Question(questionDTO.getQuestion(), questionDTO.getDescription(), questionDTO.isRequired(), questionDTO.getQuestionType(), questionDTO.isNotSureAllowed(), null, referenceId)
                             : new Question(questionDTO.getQuestion(), questionDTO.getDescription(), questionDTO.isRequired(), questionDTO.getQuestionType(), questionDTO.isNotSureAllowed(), referenceId, null);
                 }
+                if(templateType != QuestionnaireTemplateType.RISK)
                 addAttributeNameToQuestion(question, questionDTO.getAttributeName(), templateType);
                 questions.add(question);
             });
