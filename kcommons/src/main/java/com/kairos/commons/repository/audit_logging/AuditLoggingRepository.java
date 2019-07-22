016a51a02296e4f8d9415ebb5374f7ec4263cfb3
Deepak Maurya <deepak.maurya@oodlestechnologies.com>
2019-07-22 13:57:11 +0530

    Show history of a day in Shift details

--- kcommons/src/main/java/com/kairos/commons/repository/audit_logging/AuditLoggingRepository.java
+++ kcommons/src/main/java/com/kairos/commons/repository/audit_logging/AuditLoggingRepository.java
@@ -3,7 +3,10 @@ package com.kairos.commons.repository.audit_logging;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -21,4 +24,13 @@ public class AuditLoggingRepository {
     public List<Map> getAuditLoggingByType(String auditLogType){
         return mongoTemplate.findAll(Map.class,auditLogType);
     }
+    public List<Map> getAuditLogOfStaff(Long staffId,LocalDate startDate,LocalDate endDate) {
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where("staffId").is(staffId).and ("startDate").gte(startDate).and("endDate").lte(endDate));
+        List<Map> shifts = mongoTemplate.find(query ,Map.class,"Shift");
+        return shifts;
+    }
 }
+
+
