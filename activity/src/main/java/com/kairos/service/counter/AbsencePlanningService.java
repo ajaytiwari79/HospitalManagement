2422a93dec9d4e25338e0b341d0e83366ff95a54
suraj21090 <suraj.verma@oodlestechnologies.com>
2019-08-07 13:44:40 +0530

    KP-7542

---
+++ activity/src/main/java/com/kairos/service/counter/AbsencePlanningService.java
@@ -0,0 +1,77 @@
+package com.kairos.service.counter;
+
+
+import com.kairos.commons.utils.DateTimeInterval;
+import com.kairos.dto.activity.counter.chart.CommonKpiDataUnit;
+import com.kairos.dto.activity.kpi.StaffKpiFilterDTO;
+import com.kairos.enums.FilterType;
+import com.kairos.persistence.model.counter.ApplicableKPI;
+import com.kairos.persistence.model.period.PlanningPeriod;
+import com.kairos.persistence.model.shift.Shift;
+import com.kairos.persistence.repository.period.PlanningPeriodMongoRepository;
+import com.kairos.persistence.repository.shift.ShiftMongoRepository;
+import com.kairos.utils.counter.KPIUtils;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static com.kairos.commons.utils.DateUtils.asDate;
+import static com.kairos.commons.utils.ObjectUtils.isCollectionNotEmpty;
+import static com.kairos.utils.counter.KPIUtils.sortKpiDataByDateTimeInterval;
+
+@Service
+public class AbsencePlanningService {
+    @Inject
+    private PlanningPeriodMongoRepository planningPeriodMongoRepository;
+    @Inject
+    private CounterHelperService counterHelperService;
+    @Inject
+    private ShiftMongoRepository shiftMongoRepository;
+
+    private Map<Long, Set<DateTimeInterval>> getPlanningPeriodIntervals(List<Long> unitIds, Date startDate, Date endDate) {
+        Map<Long, Set<DateTimeInterval>> unitAndDateTimeIntervalMap = new HashMap<>();
+        List<PlanningPeriod> planningPeriods = planningPeriodMongoRepository.findAllByUnitIdsAndBetweenDates(unitIds, startDate, endDate);
+        Map<Long, List<PlanningPeriod>> unitAndPlanningPeriodMap = planningPeriods.stream().collect(Collectors.groupingBy(PlanningPeriod::getUnitId, Collectors.toList()));
+        unitIds.forEach(unitId -> {
+            Set<DateTimeInterval>  dateTimeIntervals = unitAndPlanningPeriodMap.getOrDefault(unitId , new ArrayList<>()).stream().map(planningPeriod -> new DateTimeInterval(asDate(planningPeriod.getStartDate()), asDate(planningPeriod.getEndDate()))).collect(Collectors.toSet());
+            unitAndDateTimeIntervalMap.put(unitId, dateTimeIntervals);
+        });
+        return unitAndDateTimeIntervalMap;
+    }
+
+    private List<CommonKpiDataUnit> getAbsencePlanningKpiData(Long organizationId, Map<FilterType, List> filterBasedCriteria ,ApplicableKPI applicableKPI) {
+        List<CommonKpiDataUnit> kpiDataUnits ;
+        Object[] filterCriteria = counterHelperService.getDataByFilterCriteria(filterBasedCriteria);
+        List<Long> staffIds = (List<Long>)filterCriteria[0];
+        List<LocalDate> filterDates = (List<LocalDate>)filterCriteria[1];
+        List<Long> unitIds = (List<Long>)filterCriteria[2];
+        List<Long> employmentTypeIds = (List<Long>)filterCriteria[3];
+        List<String> shiftActivityStatus = (filterBasedCriteria.get(FilterType.ACTIVITY_STATUS) != null) ? filterBasedCriteria.get(FilterType.ACTIVITY_STATUS) : new ArrayList<>();
+        if (CollectionUtils.isEmpty(unitIds)) {
+            unitIds.add(organizationId);
+        }
+        Object[] kpiData = counterHelperService.getKPIdata(applicableKPI,filterDates,staffIds,employmentTypeIds,unitIds,organizationId);
+        List<DateTimeInterval> dateTimeIntervals = (List<DateTimeInterval>)kpiData[1];
+        List<StaffKpiFilterDTO> staffKpiFilterDTOS = (List<StaffKpiFilterDTO>)kpiData[0];
+        staffIds=(List<Long>)kpiData[2];
+        List<Shift> shifts = shiftMongoRepository.findShiftsByKpiFiltersWithActivityStatus(staffIds, isCollectionNotEmpty(unitIds) ? unitIds : Arrays.asList(organizationId), shiftActivityStatus, dateTimeIntervals.get(0).getStartDate(), dateTimeIntervals.get(dateTimeIntervals.size() - 1).getEndDate());
+        List<String> absencePlanningActivityStatus = getAbsencePlanningActivityStatus();
+        kpiDataUnits = getKpiDataUnits(absencePlanningActivityStatus,applicableKPI,staffKpiFilterDTOS);
+        sortKpiDataByDateTimeInterval(kpiDataUnits);
+        return kpiDataUnits;
+    }
+
+    public List<String> getAbsencePlanningActivityStatus()
+    {
+        return null;
+    }
+
+
+
+
+}
