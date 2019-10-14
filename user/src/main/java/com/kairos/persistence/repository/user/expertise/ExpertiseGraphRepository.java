11d90971fc61d9e6d33eea818b11d568f5d5c6c6
Harish Modi <harish@harish.modi>
2019-10-14 18:30:56 +0530

    fix changes in kpi date format

--- user/src/main/java/com/kairos/persistence/repository/user/expertise/ExpertiseGraphRepository.java
+++ user/src/main/java/com/kairos/persistence/repository/user/expertise/ExpertiseGraphRepository.java
@@ -247,7 +247,8 @@ public interface ExpertiseGraphRepository extends Neo4jBaseRepository<Expertise,
             "RETURN id(expertise) as id,expertise.name as name, collect(id(unit)) as supportedUnitIds")
     List<ExpertiseQueryResult> findAllExpertiseWithUnitIds();
 
-    @Query("MATCH(expertise:Expertise{deleted:false,published:true})-[:"+HAS_PROTECTED_DAYS_OFF_SETTINGS+"]->(protectedSetting:ProtectedDaysOffSetting)\n" +
+    @Query("MATCH(expertise:Expertise{deleted:false,published:true}) WHERE id(expertise) = {0}" +
+            "MATCH(expertise)-[:"+HAS_PROTECTED_DAYS_OFF_SETTINGS+"]->(protectedSetting:ProtectedDaysOffSetting)\n" +
             "RETURN protectedSetting")
     List<ProtectedDaysOffSetting> findProtectedDaysOffSettingByExpertiseId(Long ExpertiseId);
 
