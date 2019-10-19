0be413b732ef012ff836053afdee96863e54aa30
Harish Modi <harish@harish.modi>
2019-10-19 10:33:26 +0530

    add cost calculation kpi service

--- activity/src/main/java/com/kairos/service/counter/CostCalculationKPIService.java
+++ activity/src/main/java/com/kairos/service/counter/CostCalculationKPIService.java
@@ -1,14 +1,17 @@
 package com.kairos.service.counter;
 
 import com.kairos.dto.activity.kpi.StaffKpiFilterDTO;
+import com.kairos.dto.user.employment.EmploymentLinesDTO;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class CostCalculationKPIService {
 
-  public void calculateTotalCostOfStaff(List<StaffKpiFilterDTO> staffKpiFilterDTOS){
+  public void calculateTotalCostOfStaff(List<StaffKpiFilterDTO> staffKpiFilterDTOS, LocalDate startDate ,LocalDate endDate){
+      Map<Long , Map<LocalDate, > >
 
 
 
