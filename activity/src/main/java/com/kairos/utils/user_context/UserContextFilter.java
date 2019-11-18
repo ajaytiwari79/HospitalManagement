e5534a5333a6a50ff3b13ccc7a133a9d850a233b
pavanoodles <pavan.kumar@oodlestechnologies.com>
2019-11-18 13:58:30 +0530

    Added fixes of duplicate expertise

---
+++ activity/src/main/java/com/kairos/utils/user_context/UserContextFilter.java
@@ -0,0 +1,41 @@
+package com.kairos.utils.user_context;
+
+import com.kairos.dto.user_context.UserContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class UserContextFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(UserContextFilter.class);
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        UserContext.setCorrelationId(  httpServletRequest.getHeader(UserContext.CORRELATION_ID) );
+        UserContext.setUserId( httpServletRequest.getHeader(UserContext.USER_ID) );
+        UserContext.setAuthToken( httpServletRequest.getHeader(UserContext.AUTH_TOKEN) );
+        long time = System.currentTimeMillis();
+        try {
+            filterChain.doFilter(httpServletRequest, servletResponse);
+        } finally {
+            time = System.currentTimeMillis() - time;
+            logger.info("time taken {}: {} ms ", httpServletRequest.getRequestURI(),  time);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        //Not in use
+    }
+
+    @Override
+    public void destroy() {
+        //Not in use
+    }
+}
