08e22aa1f405e7e51021b5a01f55785e72151b54
Gaurav Ranjan <gauravranjan@Gaurav.Ranjan>
2019-11-27 15:21:23 +0530

    fix Child Care Days Check issue

--- activity/src/main/java/com/kairos/persistence/model/wta/templates/template_types/ChildCareDaysCheckWTATemplate.java
+++ activity/src/main/java/com/kairos/persistence/model/wta/templates/template_types/ChildCareDaysCheckWTATemplate.java
@@ -8,7 +8,9 @@ import com.kairos.dto.activity.shift.ShiftWithActivityDTO;
 import com.kairos.dto.activity.shift.WorkTimeAgreementRuleViolation;
 import com.kairos.dto.user.expertise.CareDaysDTO;
 import com.kairos.enums.DurationType;
+import com.kairos.enums.TimeTypeEnum;
 import com.kairos.enums.wta.WTATemplateType;
+import com.kairos.persistence.model.activity.ActivityWrapper;
 import com.kairos.persistence.model.wta.templates.WTABaseRuleTemplate;
 import com.kairos.wrapper.wta.RuleTemplateSpecificInfo;
 import lombok.Getter;
@@ -18,11 +20,11 @@ import org.apache.commons.collections.CollectionUtils;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static com.kairos.commons.utils.ObjectUtils.*;
-import static com.kairos.utils.worktimeagreement.RuletemplateUtils.getCareDays;
 import static com.kairos.utils.worktimeagreement.RuletemplateUtils.getIntervalByActivity;
 
 /**
@@ -45,8 +47,7 @@ public class ChildCareDaysCheckWTATemplate extends WTABaseRuleTemplate {
 
     @Override
     public void validateRules(RuleTemplateSpecificInfo infoWrapper) {
-        if (!isDisabled()) {
-            //CareDaysDTO careDays = getCareDays(infoWrapper.getChildCareDays(), infoWrapper.getStaffAge());
+        if (!isDisabled() && validateRulesChildCareDayCheck(infoWrapper.getActivityWrapperMap())) {
             if (isCollectionNotEmpty(infoWrapper.getChildCareDays())) {
                 long leaveCount = calculateChildCareDaysLeaveCount(infoWrapper.getChildCareDays(), infoWrapper.getStaffChildAges());
                 DateTimeInterval dateTimeInterval = getIntervalByActivity(infoWrapper.getActivityWrapperMap(), infoWrapper.getShift().getStartDate(), activityIds);
@@ -62,6 +63,15 @@ public class ChildCareDaysCheckWTATemplate extends WTABaseRuleTemplate {
             }
         }
 
+    }
+
+    private boolean validateRulesChildCareDayCheck(Map<BigInteger, ActivityWrapper> activityWrapperMap) {
+        for(BigInteger activityId : activityWrapperMap.keySet()){
+            if(!TimeTypeEnum.PAID_BREAK.equals(activityWrapperMap.get(activityId).getTimeTypeInfo().getSecondLevelType()) && isNotNull(activityWrapperMap.get(activityId).getActivity().getRulesActivityTab().getCutOffIntervalUnit())){
+                return true;
+            }
+        }
+        return false;
     }
 
     public long calculateChildCareDaysLeaveCount(List<CareDaysDTO> careDaysDTOS, List<Integer> staffChildAges){
