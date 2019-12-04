e0af3836d983bea7420ed89604f1c180fcfea703
pavanoodles <pavan.kumar@oodlestechnologies.com>
2019-12-04 17:57:54 +0530

    Added fixes of staff activity settings

--- kcommons/src/main/java/com/kairos/commons/utils/ObjectUtils.java
+++ kcommons/src/main/java/com/kairos/commons/utils/ObjectUtils.java
@@ -77,5 +77,12 @@ public class ObjectUtils {
         return hoursValue + "." + minutesValue;
     }
 
+    public static boolean isEquals(Object o1, Object o2) {
+        if (o1 != null && o2 != null) {
+            return o1.equals(o2);
+        }
+        return o1 == null && o2 == null;
+    }
+
 
 }
