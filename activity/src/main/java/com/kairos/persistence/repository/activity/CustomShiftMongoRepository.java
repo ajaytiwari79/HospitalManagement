3db6a842d4208e07651b43452c9cbdd2452c0308
pradeep <pradeep.singh@oodlestechnologies.com>
2019-12-26 19:07:22 +0530

    timebank issue fix

--- activity/src/main/java/com/kairos/persistence/repository/activity/CustomShiftMongoRepository.java
+++ activity/src/main/java/com/kairos/persistence/repository/activity/CustomShiftMongoRepository.java
@@ -88,5 +88,6 @@ public interface CustomShiftMongoRepository {
     List<Shift> findShiftByShiftActivityIdAndBetweenDate(Collection<BigInteger> shiftActivityIds,LocalDate startDate,LocalDate endDate,Long staffId);
 
     List<ShiftWithActivityDTO> findAllShiftsBetweenDurationByEmploymentIdNotEqualShiftIds(Long employmentId, Date startDate, Date endDate,List<BigInteger> shiftIds);
+    List<ShiftWithActivityDTO> findAllShiftsBetweenDurationByEmploymentIds(Collection<Long> employmentIds, Date startDate, Date endDate,Boolean draftShift);
 
 }
