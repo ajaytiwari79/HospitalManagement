156c92037bcef001560f3fdf9fa44ff00b2a17fd
Suraj Verma <suraj@suraj.verma>
2020-01-20 13:08:50 +0530

    KP-8743

--- activity/src/main/java/com/kairos/service/counter/AbsencePlanningKPIService.java
+++ activity/src/main/java/com/kairos/service/counter/AbsencePlanningKPIService.java
@@ -159,10 +159,10 @@ public class AbsencePlanningKPIService implements CounterService {
                     break;
             }
         }
-        clusteredBarChartKpiDataUnits.add(new ClusteredBarChartKpiDataUnit("Requested", AppConstants.REQUESTED_COLOR_CODE, getValueOfTodo(todoDTOS,xAxisConfig,requested)));
-        clusteredBarChartKpiDataUnits.add(new ClusteredBarChartKpiDataUnit("Pending", AppConstants.PENDING_COLOR_CODE,getValueOfTodo(todoDTOS,xAxisConfig,pending)));
-        clusteredBarChartKpiDataUnits.add(new ClusteredBarChartKpiDataUnit("Approve", AppConstants.APPROVE_COLOR_CODE, getValueOfTodo(todoDTOS,xAxisConfig,approve)));
-        clusteredBarChartKpiDataUnits.add(new ClusteredBarChartKpiDataUnit("Disapprove", AppConstants.DISAPPROVE_COLOR_CODE,getValueOfTodo(todoDTOS,xAxisConfig,disapprove)));
+        clusteredBarChartKpiDataUnits.add(new ClusteredBarChartKpiDataUnit(AppConstants.REQUESTED, AppConstants.REQUESTED_COLOR_CODE, getValueOfTodo(todoDTOS,xAxisConfig,requested)));
+        clusteredBarChartKpiDataUnits.add(new ClusteredBarChartKpiDataUnit(AppConstants.PENDING, AppConstants.PENDING_COLOR_CODE,getValueOfTodo(todoDTOS,xAxisConfig,pending)));
+        clusteredBarChartKpiDataUnits.add(new ClusteredBarChartKpiDataUnit(AppConstants.APPROVE, AppConstants.APPROVE_COLOR_CODE, getValueOfTodo(todoDTOS,xAxisConfig,approve)));
+        clusteredBarChartKpiDataUnits.add(new ClusteredBarChartKpiDataUnit(AppConstants.DISAPPROVE, AppConstants.DISAPPROVE_COLOR_CODE,getValueOfTodo(todoDTOS,xAxisConfig,disapprove)));
 
         return clusteredBarChartKpiDataUnits;
     }
