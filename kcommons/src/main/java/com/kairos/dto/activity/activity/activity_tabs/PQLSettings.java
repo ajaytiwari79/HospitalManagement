b93880bf48d5a0377019da2770d5ea9f5329a4be
Suraj Verma <suraj@suraj.verma>
2020-01-22 15:22:16 +0530

    KP-8921

--- kcommons/src/main/java/com/kairos/dto/activity/activity/activity_tabs/PQLSettings.java
+++ kcommons/src/main/java/com/kairos/dto/activity/activity/activity_tabs/PQLSettings.java
@@ -4,19 +4,13 @@ package com.kairos.dto.activity.activity.activity_tabs;
  *Used for Planning Quality level of planner
  */
 
+import com.kairos.constants.CommonConstants;
 import com.kairos.dto.activity.open_shift.DurationField;
 
 import java.util.Optional;
 
 public class PQLSettings {
-    public static final String GREEN_COLOR_CODE = "#4caf502e";
-    public static final String COLOR_NAME = "Green";
-    public static final String YELLOW_COLOR_CODE = "#ffeb3b33";
-    public static final String COLOR_NAME1 = "Yellow";
-    public static final String COLOR_NAME_1 = COLOR_NAME1;
-    public static final String RED_COLOR_CODE = "#ff3b3b33";
-    public static final String RED = "Red";
-    public static final String COLOR_NAME2 = RED;
+
     private DurationField approvalTimeInAdvance; // TODO need to rename
     private Float approvalPercentageWithoutMovement;
     private ApprovalCriteria approvalWithMovement;
@@ -53,9 +47,9 @@ public class PQLSettings {
     }
 
     public ApprovalCriteria getAppreciable() {
-        appreciable=Optional.ofNullable(appreciable).orElse(new ApprovalCriteria(GREEN_COLOR_CODE, COLOR_NAME));
-        appreciable.setColorName(COLOR_NAME);
-        appreciable.setColor(GREEN_COLOR_CODE);
+        appreciable=Optional.ofNullable(appreciable).orElse(new ApprovalCriteria(CommonConstants.GREEN_COLOR_CODE, CommonConstants.COLOR_NAME));
+        appreciable.setColorName(CommonConstants.COLOR_NAME);
+        appreciable.setColor(CommonConstants.GREEN_COLOR_CODE);
         return appreciable;
     }
 
@@ -64,9 +58,9 @@ public class PQLSettings {
     }
 
     public ApprovalCriteria getAcceptable() {
-        acceptable=Optional.ofNullable(acceptable).orElse(new ApprovalCriteria(YELLOW_COLOR_CODE , COLOR_NAME_1));
-        acceptable.setColorName(COLOR_NAME1);
-        acceptable.setColor(YELLOW_COLOR_CODE);
+        acceptable=Optional.ofNullable(acceptable).orElse(new ApprovalCriteria(CommonConstants.YELLOW_COLOR_CODE , CommonConstants.COLOR_NAME1));
+        acceptable.setColorName(CommonConstants.COLOR_NAME1);
+        acceptable.setColor(CommonConstants.YELLOW_COLOR_CODE);
         return acceptable;
     }
 
@@ -75,9 +69,9 @@ public class PQLSettings {
     }
 
     public ApprovalCriteria getCritical() {
-        critical=Optional.ofNullable(critical).orElse(new ApprovalCriteria( RED_COLOR_CODE, COLOR_NAME2));
-        critical.setColorName(COLOR_NAME2);
-        critical.setColor(RED_COLOR_CODE);
+        critical=Optional.ofNullable(critical).orElse(new ApprovalCriteria( CommonConstants.RED_COLOR_CODE, CommonConstants.RED));
+        critical.setColorName(CommonConstants.RED);
+        critical.setColor(CommonConstants.RED_COLOR_CODE);
         return critical;
     }
 
