56385ff657ccbeb1d8a1005ea90b2766e72a14af
pavanoodles <pavan.kumar@oodlestechnologies.com>
2020-02-15 15:36:32 +0530

    Added dynamic translatiob

--- user/src/main/java/com/kairos/persistence/model/common/UserBaseEntity.java
+++ user/src/main/java/com/kairos/persistence/model/common/UserBaseEntity.java
@@ -2,9 +2,11 @@ package com.kairos.persistence.model.common;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.kairos.dto.activity.activity.TranslationInfo;
 import lombok.Getter;
 import lombok.Setter;
 import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.Properties;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -12,6 +14,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Contains common fields of an entity
@@ -39,4 +43,16 @@ public abstract class UserBaseEntity implements Serializable {
     @JsonIgnore
     @LastModifiedBy
     protected Long lastModifiedBy;
+    @Properties
+    protected Map<String,String> translatedNames;
+    @Properties
+    protected Map<String,String> translatedDescriptions;
+
+
+    @JsonIgnore
+    public Map<String, TranslationInfo> getTranslatedData() {
+        Map<String, TranslationInfo> infoMap=new HashMap<>();
+        translatedNames.forEach((k,v)-> infoMap.put(k,new TranslationInfo(v,translatedDescriptions.get(k))));
+        return infoMap;
+    }
 }
