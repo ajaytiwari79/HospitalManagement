55ae324f0fe4eae84d14f7d09a17842c9e2d033b
Pawan Kumar <pawan@pawan.kumar>
2020-05-18 16:16:33 +0530

    Added

--- user/src/main/java/com/kairos/service/organization/TeamService.java
+++ user/src/main/java/com/kairos/service/organization/TeamService.java
@@ -139,15 +139,9 @@ public class TeamService {
     }
 
     public boolean updateActivitiesOfTeam(Long teamId, Set<BigInteger> activityIds) {
-        List<Team> teams=teamGraphRepository.findAllByDeletedFalseAndIsEnabledTrue();
-        teams.forEach(team -> {
-            if(team.getId().equals(teamId)){
-                team.setActivityIds(activityIds);
-            }else {
-                team.getActivityIds().removeAll(activityIds);
-            }
-        });
-        teamGraphRepository.saveAll(teams);
+        Team team = teamGraphRepository.findOne(teamId);
+        team.setActivityIds(activityIds);
+        teamGraphRepository.save(team);
         return true;
     }
 
