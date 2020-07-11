59b4fae17f5029bcd0df2941cd23a34a883d69c2
Pawan Kumar <pawan@pawan.kumar>
2020-07-11 15:45:29 +0530

    Added changes

--- kcommons/src/main/java/com/kairos/dto/activity/staffing_level/StaffingLevelPublishDTO.java
+++ kcommons/src/main/java/com/kairos/dto/activity/staffing_level/StaffingLevelPublishDTO.java
@@ -5,15 +5,22 @@ import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigInteger;
+import java.time.LocalDate;
 import java.util.*;
+
+import static com.kairos.commons.utils.ObjectUtils.isCollectionEmpty;
 
 @Getter
 @Setter
 public class StaffingLevelPublishDTO {
     @DateTimeFormat(pattern="yyyy-MM-dd")
-    Date startDate;
+    private Date startDate;
     @DateTimeFormat(pattern="yyyy-MM-dd")
     private Date endDate;
+    private Set<LocalDate> weekDates;
+    private LocalDate selectedDate;
+
     private Set<BigInteger> activityIds=new HashSet<>();
     private Set<Long> skillIds=new HashSet<>();
+    private boolean publishAbsence;
 }
