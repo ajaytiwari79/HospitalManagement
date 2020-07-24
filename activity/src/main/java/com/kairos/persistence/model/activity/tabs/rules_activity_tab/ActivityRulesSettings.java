bc87107d989cd9dae481f65c1ffe3d53339b5aac
Pawan Kumar <pawan@pawan.kumar>
2020-07-24 18:35:26 +0530

    Added

--- activity/src/main/java/com/kairos/persistence/model/activity/tabs/rules_activity_tab/ActivityRulesSettings.java
+++ activity/src/main/java/com/kairos/persistence/model/activity/tabs/rules_activity_tab/ActivityRulesSettings.java
@@ -30,16 +30,27 @@ public class ActivityRulesSettings {
     private boolean eligibleToBeForced;
     @KPermissionField
     private List<Long> dayTypes= new ArrayList<>();
+    @KPermissionField
     private boolean eligibleForStaffingLevel;
+    @KPermissionField
     private boolean breakAllowed;
+    @KPermissionField
     private List<BigInteger> approvalAllowedPhaseIds=new ArrayList<>();
+    @KPermissionField
     private LocalDate cutOffStartFrom;
+    @KPermissionField
     private CutOffIntervalUnit cutOffIntervalUnit;
+    @KPermissionField
     private Integer cutOffdayValue;
+    @KPermissionField
     private List<CutOffInterval> cutOffIntervals;
+    @KPermissionField
     private CutOffIntervalUnit.CutOffBalances cutOffBalances;
+    @KPermissionField
     private boolean borrowLeave;
+    @KPermissionField
     private boolean transferAll;
+    @KPermissionField
     private int noOfTransferLeave;
     // in Minutes
     @KPermissionField
@@ -51,13 +62,17 @@ public class ActivityRulesSettings {
     @KPermissionField
     private Short longestTime;
     private boolean eligibleForCopy;
+    @KPermissionField
     private DurationField plannedTimeInAdvance;
+    @KPermissionField
     private LocalTime maximumEndTime;// shift can't be extend this time
     //remove after integration
+    @KPermissionField
     private boolean allowedAutoAbsence;
     private byte recurrenceDays;// if a staff fall sick and select this activity then for recurrence days and times --   -128 to 127
     private byte recurrenceTimes;// -- the  shift of that staff will be entered.     -128 to 127
     private PQLSettings pqlSettings=new PQLSettings();
+    @KPermissionField
     private ReasonCodeRequiredState reasonCodeRequiredState;
     private List<BigInteger> staffTagIds;
     private SicknessSetting sicknessSetting=new SicknessSetting();
