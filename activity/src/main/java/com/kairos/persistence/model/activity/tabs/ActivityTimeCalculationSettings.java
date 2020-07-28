ca94b9024cdd48cef094c9f946efc5e70f4d9565
Pawan Kumar <pawan@pawan.kumar>
2020-07-28 17:50:01 +0530

    Added fixes

--- activity/src/main/java/com/kairos/persistence/model/activity/tabs/ActivityTimeCalculationSettings.java
+++ activity/src/main/java/com/kairos/persistence/model/activity/tabs/ActivityTimeCalculationSettings.java
@@ -21,21 +21,31 @@ public class ActivityTimeCalculationSettings {
 
     @KPermissionField
     private String methodForCalculatingTime;
+    @KPermissionField
     private TimeCalaculationType fullDayCalculationType;
+    @KPermissionField
     private TimeCalaculationType fullWeekCalculationType;
     private Boolean allowBreakReduction;
+    @KPermissionField
     private Long fixedTimeValue;
     private String methodForCalculatingTimeInMonths;
     private List<String> balanceType;
+    @KPermissionField
     private Boolean multiplyWith;
+    @KPermissionField
     private Double multiplyWithValue = ApiConstants.DEFAULT_VALUE;
     private Boolean multiplyByVacationFactor;
     private Boolean multiplyByFinalSchedule;
+    @KPermissionField
     private DayOfWeek fullWeekStart;
+    @KPermissionField
     private DayOfWeek fullWeekEnd;
     private String breakTemplates;
+    @KPermissionField
     private int historyDuration;
+    @KPermissionField
     private LocalTime defaultStartTime;
+    @KPermissionField
     private List<Long> dayTypes = new ArrayList<>();
     private boolean replaceWithPublishedShiftTime;
     private boolean replaceWithUnapprovedAbsenceRequest;
