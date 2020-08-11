68de9586fda3b252efbdc732d93b805b9d6ca0ee
Pawan Kumar <pawan@pawan.kumar>
2020-08-11 12:46:44 +0530

    Added fixes of FLP

--- user/src/main/java/com/kairos/service/access_permisson/AccessGroupService.java
+++ user/src/main/java/com/kairos/service/access_permisson/AccessGroupService.java
@@ -889,18 +889,18 @@ public class AccessGroupService {
 
     public List<AccessGroup> validAccessGroupByDate(Long unitId,Date date){
         AccessGroupStaffQueryResult accessGroupStaffQueryResult = accessGroupRepository.getAccessGroupDayTypesAndUserId(unitId,UserContext.getUserDetails().getId());
-        if(!UserContext.getUserDetails().isHubMember() && isNull(accessGroupStaffQueryResult)){
-            exceptionService.actionNotPermittedException(ERROR_POSITION_ACCESSGROUP_NOTFOUND);
-        }
         List<AccessGroup> accessGroups = new ArrayList<>();
-        List<AccessGroupDayTypesQueryResult> accessGroupDayTypesQueryResults = ObjectMapperUtils.copyCollectionPropertiesByMapper(accessGroupStaffQueryResult.getDayTypesByAccessGroup(),AccessGroupDayTypesQueryResult.class);
-        for (AccessGroupDayTypesQueryResult accessGroupDayTypesQueryResult : accessGroupDayTypesQueryResults) {
-            if(isNotNull(accessGroupDayTypesQueryResult.getAccessGroup())){
-                if(!accessGroupDayTypesQueryResult.getAccessGroup().isAllowedDayTypes() && isDayTypeValid(date,accessGroupDayTypesQueryResult.getDayTypes()));{
-                    accessGroups.add(accessGroupDayTypesQueryResult.getAccessGroup());
+        if(!UserContext.getUserDetails().isHubMember()){
+            List<AccessGroupDayTypesQueryResult> accessGroupDayTypesQueryResults = ObjectMapperUtils.copyCollectionPropertiesByMapper(accessGroupStaffQueryResult.getDayTypesByAccessGroup(),AccessGroupDayTypesQueryResult.class);
+            for (AccessGroupDayTypesQueryResult accessGroupDayTypesQueryResult : accessGroupDayTypesQueryResults) {
+                if(isNotNull(accessGroupDayTypesQueryResult.getAccessGroup())){
+                    if(!accessGroupDayTypesQueryResult.getAccessGroup().isAllowedDayTypes() && isDayTypeValid(date,accessGroupDayTypesQueryResult.getDayTypes()));{
+                        accessGroups.add(accessGroupDayTypesQueryResult.getAccessGroup());
+                    }
                 }
             }
         }
+
         return accessGroups;
     }
 
