202ddeabacb8ac6ee94c65db94e86202131b1c05
Suraj Verma <suraj@suraj.verma>
2020-09-16 17:30:34 +0530

    KP-10588 To update translation of absence tab in country module

--- activity/src/main/java/com/kairos/persistence/model/open_shift/OpenShiftRuleTemplateDTO.java
+++ activity/src/main/java/com/kairos/persistence/model/open_shift/OpenShiftRuleTemplateDTO.java
@@ -2,18 +2,24 @@ package com.kairos.persistence.model.open_shift;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.kairos.dto.TranslationInfo;
 import com.kairos.dto.activity.open_shift.ActivitiesPerTimeType;
 import com.kairos.dto.activity.open_shift.PlannerNotificationInfo;
 import com.kairos.dto.activity.open_shift.Priority;
 import com.kairos.dto.activity.open_shift.ShiftAssignmentCriteria;
 import com.kairos.enums.AllowedLength;
 import com.kairos.enums.OpenShiftRuleTemplateType;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
 public class OpenShiftRuleTemplateDTO {
     private BigInteger id;
     private String name;
@@ -36,177 +42,10 @@ public class OpenShiftRuleTemplateDTO {
     private PlannerNotificationInfo plannerNotificationInfo;
     private Priority priority;
     private OpenShiftInterval openShiftInterval;
+    private  Map<String, TranslationInfo> translations;
 
 
     public OpenShiftRuleTemplateDTO() {
         //Default Constructor
-    }
-
-    public OpenShiftInterval getOpenShiftInterval() {
-        return openShiftInterval;
-    }
-
-    public void setOpenShiftInterval(OpenShiftInterval openShiftInterval) {
-        this.openShiftInterval = openShiftInterval;
-    }
-
-    public BigInteger getId() {
-        return id;
-    }
-
-    public void setId(BigInteger id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getOrganizationTypeId() {
-        return organizationTypeId;
-    }
-
-    public void setOrganizationTypeId(Long organizationTypeId) {
-        this.organizationTypeId = organizationTypeId;
-    }
-
-    public Long getOrganizationSubTypeId() {
-        return organizationSubTypeId;
-    }
-
-    public void setOrganizationSubTypeId(Long organizationSubTypeId) {
-        this.organizationSubTypeId = organizationSubTypeId;
-    }
-
-    public List<ActivitiesPerTimeType> getActivitiesPerTimeTypes() {
-        return activitiesPerTimeTypes;
-    }
-
-    public void setActivitiesPerTimeTypes(List<ActivitiesPerTimeType> activitiesPerTimeTypes) {
-        this.activitiesPerTimeTypes = activitiesPerTimeTypes;
-    }
-
-    public List<Long> getSelectedSkills() {
-        return selectedSkills;
-    }
-
-    public void setSelectedSkills(List<Long> selectedSkills) {
-        this.selectedSkills = selectedSkills;
-    }
-
-    public boolean isPromptPlanner() {
-        return promptPlanner;
-    }
-
-    public void setPromptPlanner(boolean promptPlanner) {
-        this.promptPlanner = promptPlanner;
-    }
-
-    public Long getUnitId() {
-        return unitId;
-    }
-
-    public void setUnitId(Long unitId) {
-        this.unitId = unitId;
-    }
-
-    public Long getCountryId() {
-        return countryId;
-    }
-
-    public void setCountryId(Long countryId) {
-        this.countryId = countryId;
-    }
-
-    public BigInteger getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(BigInteger parentId) {
-        this.parentId = parentId;
-    }
-
-    public FeatureRules getFeatureRules() {
-        return featureRules;
-    }
-
-    public void setFeatureRules(FeatureRules featureRules) {
-        this.featureRules = featureRules;
-    }
-
-    public NotificationWay getNotificationWay() {
-        return notificationWay;
-    }
-
-    public void setNotificationWay(NotificationWay notificationWay) {
-        this.notificationWay = notificationWay;
-    }
-
-    public AllowedLength getAllowedLength() {
-        return allowedLength;
-    }
-
-    public void setAllowedLength(AllowedLength allowedLength) {
-        this.allowedLength = allowedLength;
-    }
-
-    public Integer getMinimumShiftHours() {
-        return minimumShiftHours;
-    }
-
-    public void setMinimumShiftHours(Integer minimumShiftHours) {
-        this.minimumShiftHours = minimumShiftHours;
-    }
-
-    public Integer getMaximumShiftHours() {
-        return maximumShiftHours;
-    }
-
-    public void setMaximumShiftHours(Integer maximumShiftHours) {
-        this.maximumShiftHours = maximumShiftHours;
-    }
-
-    public ShiftAssignmentCriteria getShiftAssignmentCriteria() {
-        return shiftAssignmentCriteria;
-    }
-
-    public void setShiftAssignmentCriteria(ShiftAssignmentCriteria shiftAssignmentCriteria) {
-        this.shiftAssignmentCriteria = shiftAssignmentCriteria;
-    }
-
-    public BigInteger getOpenShiftIntervalId() {
-        return openShiftIntervalId;
-    }
-
-    public void setOpenShiftIntervalId(BigInteger openShiftIntervalId) {
-        this.openShiftIntervalId = openShiftIntervalId;
-    }
-
-    public PlannerNotificationInfo getPlannerNotificationInfo() {
-        return plannerNotificationInfo;
-    }
-
-    public void setPlannerNotificationInfo(PlannerNotificationInfo plannerNotificationInfo) {
-        this.plannerNotificationInfo = plannerNotificationInfo;
-    }
-
-    public OpenShiftRuleTemplateType getRuleTemplateType() {
-        return ruleTemplateType;
-    }
-
-    public void setRuleTemplateType(OpenShiftRuleTemplateType ruleTemplateType) {
-        this.ruleTemplateType = ruleTemplateType;
-    }
-
-    public Priority getPriority() {
-        return priority;
-    }
-
-    public void setPriority(Priority priority) {
-        this.priority = priority;
     }
 }
