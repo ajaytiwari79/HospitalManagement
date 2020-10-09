dfb8064b0540acd304ee1ecddc67296a8e1b76ca
Pawan Kumar <pawan@pawan.kumar>
2020-10-09 15:32:11 +0530

    Added

---
+++ activity/src/main/java/com/kairos/persistence/model/day_type/CountryHolidayCalender.java
@@ -0,0 +1,30 @@
+package com.kairos.persistence.model.day_type;
+
+import com.kairos.persistence.model.common.MongoBaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Getter
+@Setter
+@Document
+public class CountryHolidayCalender extends MongoBaseEntity {
+    private String holidayTitle;
+    private LocalDate holidayDate;
+    private BigInteger dayTypeId;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private boolean reOccuring;
+    private String description;
+    private String holidayType;
+    private boolean isEnabled = true;
+    private String googleCalId;
+    private Long countryId;
+}
