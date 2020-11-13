2668938c5a234b895f18fe581d7a8c13d874cda0
Pradeep Singh <pradeep@pradeep.singh>
2020-11-13 10:15:34 +0530

    token issue fixed

--- user/src/main/java/com/kairos/persistence/model/auth/UserPrincipal.java
+++ user/src/main/java/com/kairos/persistence/model/auth/UserPrincipal.java
@@ -74,7 +74,7 @@ public class UserPrincipal implements UserDetails, Authentication {
         details.setAge(this.user.getAge());
         details.setLastSelectedOrganizationId(this.getUser().getLastSelectedOrganizationId());
         details.setCountryId(this.getUser().getCountryId());
-        details.setUnitWiseAccessRole(this.getUser().getUnitWiseAccessRole());
+        //details.setUnitWiseAccessRole(this.getUser().getUnitWiseAccessRole());
         //details.setHubMember(this.user.getHubMember());
         details.setSystemAdmin(this.user.getSystemAdmin());
         details.setLastSelectedOrganizationCategory(this.user.getLastSelectedOrganizationCategory());
