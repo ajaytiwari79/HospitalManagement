2f7d3a8743d5bf0676dc85a36a16ad703306bf6a
oodlesgaurav <gauravprakashranjan@gmail.com>
2020-12-14 17:13:44 +0530

    added fixed

--- activity/src/main/java/com/kairos/persistence/repository/gap_settings/AutoFillGapSettingsMongoRepository.java
+++ activity/src/main/java/com/kairos/persistence/repository/gap_settings/AutoFillGapSettingsMongoRepository.java
@@ -2,7 +2,7 @@ package com.kairos.persistence.repository.gap_settings;
 
 import com.kairos.persistence.model.auto_gap_fill_settings.AutoFillGapSettings;
 import com.kairos.persistence.repository.custom_repository.MongoBaseRepository;
-import org.springframework.data.neo4j.annotation.Query;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
