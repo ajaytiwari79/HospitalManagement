d3a16c643a9ee84260f5adbb6238183189d90390
oodlesgaurav <gauravprakashranjan@gmail.com>
2020-12-17 10:14:49 +0530

    added fixed

--- activity/src/main/java/com/kairos/persistence/repository/gap_settings/AutoFillGapSettingsMongoRepository.java
+++ activity/src/main/java/com/kairos/persistence/repository/gap_settings/AutoFillGapSettingsMongoRepository.java
@@ -17,9 +17,9 @@ public interface AutoFillGapSettingsMongoRepository extends MongoBaseRepository<
     @Query("{'deleted' : false,'unitId':?0}")
     List<AutoFillGapSettings> getAllByUnitId(Long unitId);
 
-    @Query("{'deleted' : false,'unitId':?0,'organizationTypeId':?1,'organizationSubTypeId':?2,'phaseId':?3}")
-    AutoFillGapSettings getCurrentlyApplicableGapSettingsForUnit(Long unitId, Long organizationTypeId, Long organizationSubTypeId, BigInteger phaseId);
+    @Query("{'deleted' : false,'unitId':?0,'organizationTypeId':?1,'organizationSubTypeId':?2,'phaseId':?3,'gapFillingScenario':?4,'id':{$ne:?5},'gapApplicableFor':?6}")
+    AutoFillGapSettings getCurrentlyApplicableGapSettingsForUnit(Long unitId, Long organizationTypeId, Long organizationSubTypeId, BigInteger phaseId, String gapFillingScenario, BigInteger id, String gapApplicableFor);
 
-    @Query("{'deleted' : false,'countryId':?0,'organizationTypeId':?1,'organizationSubTypeId':?2,'phaseId':?3}")
-    AutoFillGapSettings getCurrentlyApplicableGapSettingsForCountry(Long countryId, Long organizationTypeId, Long organizationSubTypeId, BigInteger phaseId);
+    @Query("{'deleted' : false,'countryId':?0,'organizationTypeId':?1,'organizationSubTypeId':?2,'phaseId':?3,'gapFillingScenario':?4,'id':{$ne:?5},'gapApplicableFor':?6}")
+    AutoFillGapSettings getCurrentlyApplicableGapSettingsForCountry(Long countryId, Long organizationTypeId, Long organizationSubTypeId, BigInteger phaseId, String gapFillingScenario, BigInteger id, String gapApplicableFor);
 }
