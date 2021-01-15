1aec70a7d68625419e623696c7384767657d3c49
Pradeep Singh <pradeep@pradeep.singh>
2021-01-15 11:57:42 +0530

    organisation service api issue fixed

--- user/src/main/java/com/kairos/persistence/model/common/UserBaseEntity.java
+++ user/src/main/java/com/kairos/persistence/model/common/UserBaseEntity.java
@@ -14,6 +14,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,7 +27,7 @@ import java.util.Map;
 
 @Getter
 @Setter
-public abstract class UserBaseEntity  {
+public abstract class UserBaseEntity  implements Serializable {
 
     //@GeneratedValue
     @GraphId
