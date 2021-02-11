521fc34897971dbe97f24a6fcf9c7f5894b29c28
Pradeep Singh <pradeep@pradeep.singh>
2021-02-11 12:24:20 +0530

    caching issue fixed

--- activity/src/main/java/com/kairos/service/kpermissions/ActivityPermissionService.java
+++ activity/src/main/java/com/kairos/service/kpermissions/ActivityPermissionService.java
@@ -44,7 +44,7 @@ public class ActivityPermissionService {
     @Inject private CommonRepository commonRepository;
     @Inject private UserIntegrationService userIntegrationService;
 
-    //@Cacheable(value = "getActivityPermissionMap", key = "{#unitId, #userId}", cacheManager = "cacheManager")
+    @Cacheable(value = "getActivityPermissionMap", key = "{#unitId, #userId}", cacheManager = "cacheManager")
     public Map<String, Set<FieldLevelPermission>> getActivityPermissionMap(Long unitId, Long userId){
         FieldPermissionUserData fieldPermissionUserData=userIntegrationService.getPermissionData(newHashSet("Activity"));
         Map<String,Set<FieldLevelPermission>> fieldPermissionMap=new HashMap<>();
