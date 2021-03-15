5b02a232113960ba828339d3183a2345235b9ec7
Pradeep Singh <pradeep@pradeep.singh>
2021-03-15 14:07:17 +0530

    timetype rules api performance issue fixed

--- activity/src/main/java/com/kairos/service/organization/OrganizationActivityService.java
+++ activity/src/main/java/com/kairos/service/organization/OrganizationActivityService.java
@@ -95,6 +95,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -940,6 +941,7 @@ public class OrganizationActivityService {
 
         }
     }
+    @Async
     public Set<BigInteger> updateColorInActivity(TimeTypeDTO timeTypeDTO,BigInteger timeTypeId) {
         List<Activity> activities = activityMongoRepository.findAllByTimeTypeId(timeTypeId);
         if (isCollectionNotEmpty(activities)) {
