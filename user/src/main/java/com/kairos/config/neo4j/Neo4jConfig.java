0764146c9b515f6a0c0127312994e53b89601583
Pawan Kumar <pawan@pawan.kumar>
2021-05-13 14:40:00 +0530

    Added API for fetching shifts by ids

--- user/src/main/java/com/kairos/config/neo4j/Neo4jConfig.java
+++ user/src/main/java/com/kairos/config/neo4j/Neo4jConfig.java
@@ -53,9 +53,7 @@ public class Neo4jConfig  implements EnvironmentAware {
     /**
      * Bean for environment variables
      *
-     * @return mkkkkkkkkkkkkkkkvt8h  dfxcd/'nilg nv hvg8bi mub kh
-     *
-     *
+     * @return
      */
     @Bean
     public EnvConfig envConfig() {
