311a761aa39e5d769e66567e47d9fb0efe127c32
Pradeep Singh <pradeep@pradeep.singh>
2021-06-09 12:56:52 +0530

    API changes

--- kcommons/src/main/java/com/kairos/enums/shift/ShiftStatus.java
+++ kcommons/src/main/java/com/kairos/enums/shift/ShiftStatus.java
@@ -1,11 +1,14 @@
 package com.kairos.enums.shift;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static com.kairos.commons.utils.ObjectUtils.newHashSet;
 
 /**
  * Created by vipul on 8/5/18.
@@ -21,7 +24,8 @@ public enum ShiftStatus implements Serializable {
         return null;
 
     }
+
     public static Set<ShiftStatus> getAllStatusExceptRequestAndPending() {
-        return Stream.of(ShiftStatus.values()).filter(current->(!(current.equals(REQUEST)|| current.equals(UNPUBLISH) || current.equals(MOVED) || current.equals(REJECT)||current.equals(PUBLISH)))).collect(Collectors.toSet());
+        return newHashSet(FIX,LOCK,UNLOCK,UNFIX,PENDING,VALIDATE, DISAPPROVE,APPROVE);
     }
 }
