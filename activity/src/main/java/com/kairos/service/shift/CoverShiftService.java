3d94a5cf99ab84e9c96d5f59a2867ee14573c0d0
Pradeep Singh <pradeep@pradeep.singh>
2021-07-21 13:44:41 +0530

    cover shift issue fixed

--- activity/src/main/java/com/kairos/service/shift/CoverShiftService.java
+++ activity/src/main/java/com/kairos/service/shift/CoverShiftService.java
@@ -302,6 +302,7 @@ public class CoverShiftService {
 
         }
         coverShift.getInterestedStaffs().put(staffId, new StaffInterest(DateUtils.getDate(),staffInterest.getAccountType()));
+        coverShift.getRequestedStaffs().remove(staffId);
         coverShift.getDeclinedStaffIds().remove(staffId);
         coverShiftMongoRepository.save(coverShift);
     }
@@ -399,6 +400,9 @@ public class CoverShiftService {
         Map<BigInteger, Shift> shiftMap = shifts.stream().collect(Collectors.toMap(MongoBaseEntity::getId,Function.identity()));
         for (CoverShiftDTO coverShiftDTO : coverShiftDTOS) {
             Shift shift = shiftMap.get(coverShiftDTO.getShiftId());
+            if(isNull(shift)){
+                exceptionService.dataNotFoundByIdException(MESSAGE_SHIFT_ID, coverShiftDTO.getShiftId());
+            }
             shift.setStaffId(staffAdditionalInfoDTO.getId());
             shift.setEmploymentId(staffAdditionalInfoDTO.getEmployment().getId());
             DailyTimeBankEntry dailyTimeBankEntry = timeBankCalculationService.renewDailyTimeBank(staffAdditionalInfoDTO,shift , false);
