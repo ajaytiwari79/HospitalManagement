bb61df5913e7648c836e5483aa7d0b1f4640b6f3
oodlesgaurav <gauravprakashranjan@gmail.com>
2021-07-22 10:38:38 +0530

    added fixed

--- kcommons/src/main/java/com/kairos/dto/user/country/agreement/cta/cta_response/SectorWiseDayTypeInfo.java
+++ kcommons/src/main/java/com/kairos/dto/user/country/agreement/cta/cta_response/SectorWiseDayTypeInfo.java
@@ -16,4 +16,9 @@ public class SectorWiseDayTypeInfo implements Serializable {
     private String holidayType;
     private LocalTime startTime;
     private LocalTime endTime;
+    private Type type=Type.FLOATING;
+
+    public enum Type {
+        FIXED,FLOATING
+    }
 }
