e9175cd1d9d0ec0d6f67e8cd4c7f97971c11abdf
oodlesgaurav <gaurav.ranjan@oodles.io>
2021-09-14 12:09:34 +0530

    Manage Filters

--- kcommons/src/main/java/com/kairos/enums/shift/ShiftDeletedBy.java
+++ kcommons/src/main/java/com/kairos/enums/shift/ShiftDeletedBy.java
@@ -1,6 +1,29 @@
 package com.kairos.enums.shift;
 
-public enum ShiftDeletedBy {
-    STAFF,MANAGEMENT,SYSTEM;
+import com.kairos.dto.user.country.filter.FilterDetailDTO;
 
+import java.util.*;
+
+public enum ShiftDeletedBy {
+    STAFF("Staff"), MANAGEMENT("Management"), SYSTEM("System");
+    private String shiftDeletedBy;
+
+    ShiftDeletedBy() {}
+
+    ShiftDeletedBy(String accessGroupRole) {
+        this.shiftDeletedBy = accessGroupRole;
+    }
+
+    public static Set<ShiftDeletedBy> getAllShiftDeleteBy() {
+        return new HashSet<>(EnumSet.allOf(ShiftDeletedBy.class));
+    }
+
+    public static List<FilterDetailDTO> getListOfShiftDeleteByForFilters(){
+        List<FilterDetailDTO> shiftDeletedByFilterData = new ArrayList<>();
+        for(ShiftDeletedBy shiftDeletedBy : EnumSet.allOf(ShiftDeletedBy.class)){
+            FilterDetailDTO filterDetailDTO = new FilterDetailDTO(shiftDeletedBy.name(), shiftDeletedBy.shiftDeletedBy);
+            shiftDeletedByFilterData.add(filterDetailDTO);
+        }
+        return shiftDeletedByFilterData;
+    }
 }
